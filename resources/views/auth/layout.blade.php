663885f76eb5cbdbcaddeee7414a9b39b89b4206
Mario Pérez Esteso <mario@geekytheory.com>
2016-03-06 18:00:55 +0100

    Fix bootstrap import

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
-    {!! Html::style('assets/css/bootstrap.css') !!}
+    {!! Html::style('assets/css/bootstrap/bootstrap.css') !!}
             <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -65,8 +65,7 @@
 @endif
 
 @yield('content')
-
             <!-- Scripts -->
-    {!! Html::script('assets/js/bootstrap.min.js') !!}
+    {!! Html::script('assets/js/bootstrap/bootstrap.min.js') !!}
 </body>
 </html>
