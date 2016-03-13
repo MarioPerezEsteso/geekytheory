f22ee274bd72529191d74eb1f95195cd3dc2e0ce
Mario Pérez Esteso <mario@geekytheory.com>
2016-03-13 22:03:48 +0100

    Add helper to get pagination text

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -4,12 +4,25 @@
  * Returns class 'active' if the route request matches
  * with the menu item
  *
- * @param $path
+ * @param string $path
  * @return string
  */
 if (!function_exists('classActiveRoute')) {
     function classActiveRoute($path)
     {
         return Request::is($path) ? ' active ' : '';
+    }
+}
+
+/**
+ * Returns pagination text
+ *
+ * @param Illuminate\Pagination\LengthAwarePaginator $paginator
+ * @return string
+ */
+if (!function_exists('getPaginationText')) {
+    function getPaginationText($paginator)
+    {
+        return trans('home.showing') . ' ' . (($paginator->currentPage() - 1) * $paginator->perPage() + 1) . ' ' . trans('home.to') . ' ' . min($paginator->total(), $paginator->currentPage() * $paginator->perPage()) . ' ' . trans('home.of') . ' ' . $paginator->total() . ' ' . trans('home.posts_min');
     }
 }
