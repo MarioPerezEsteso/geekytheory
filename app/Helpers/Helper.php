81fe8bd538fe58620c32a2a052b3bbf9544af762
Mario Pérez Esteso <mario@geekytheory.com>
2016-03-14 22:33:22 +0100

    Add helper method to create slugs

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -59,3 +59,40 @@ if (!function_exists('getStatusLabelClass')) {
         return $statusClass;
     }
 }
+
+/**
+ * Returns slug from a text
+ *
+ * @see http://stackoverflow.com/a/2955878
+ *
+ * @param string $text
+ * @return string
+ */
+if (!function_exists('slugify')) {
+    function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // remove duplicate -
+        $text = preg_replace('~-+~', '-', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+}
