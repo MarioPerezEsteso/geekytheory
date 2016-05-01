b42ddf748365d91170b32cd4ca98ec97c28b38ed
Mario Pérez Esteso <mario@geekytheory.com>
2016-05-01 16:11:17 +0200

    Create author partial and fix font file names

--- public/themes/vortex/assets/js/custom.js
+++ public/themes/vortex/assets/js/custom.js
@@ -277,47 +277,6 @@
 		});
 
 		/* ---------------------------------------------- /*
-		 * Google Map
-		/* ---------------------------------------------- */
-
-		var mapLocation = new google.maps.LatLng(34.031428,-118.2071542,17);
-
-		var $mapis = $('#map');
-
-		if ($mapis.length > 0) {
-
-			var map;
-			map = new GMaps({
-				streetViewControl : true,
-				overviewMapControl: true,
-				mapTypeControl: true,
-				zoomControl : true,
-				panControl : true,
-				scrollwheel: false,
-				center: mapLocation,
-				el: '#map',
-				zoom: 16,
-				styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
-			});
-
-			var image = new google.maps.MarkerImage('assets/images/map-icon.png',
-				new google.maps.Size(59, 65),
-				new google.maps.Point(0, 0),
-				new google.maps.Point(24, 42)
-			);
-
-			map.addMarker({
-				position: mapLocation,
-				icon: image,
-				title: 'Vortex',
-				infoWindow: {
-					content: '<p><strong>Vortex</strong><br/>121 Somewhere Ave, Suite 123<br/>P: (123) 456-7890<br/>Australia</p>'
-				}
-			});
-
-		}
-
-		/* ---------------------------------------------- /*
 		 * Ajax options
 		/* ---------------------------------------------- */
 
--- resources/views/themes/vortex/blog/layout.blade.php
+++ resources/views/themes/vortex/blog/layout.blade.php
@@ -100,28 +100,7 @@
                     </article>
 
                     <!-- AUTHOR -->
-                    <div class="post-author">
-                        <h4 class="post-author-title font-alt">Author</h4>
-                        <hr class="divider m-b-30">
-
-                        <div class="author-bio">
-                            <div class="author-avatar">
-                                <img src="https://s3.amazonaws.com/uifaces/faces/twitter/draganbabic/128.jpg" alt="">
-                            </div>
-                            <div class="author-content">
-                                <h5 class="author-name font-alt">Mark Stone</h5>
-                                <p>It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is.</p>
-                                <ul class="social-icon-links socicon-round">
-                                    <li><a href="blog-single.html#" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                                    <li><a href="blog-single.html#" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="blog-single.html#" target="_blank"><i class="fa fa-dribbble"></i></a></li>
-                                    <li><a href="blog-single.html#" target="_blank"><i class="fa fa-pinterest"></i></a></li>
-                                    <li><a href="blog-single.html#" target="_blank"><i class="fa fa-behance"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                    </div>
+                    @include('themes.vortex.partials.blog.author')
                     <!-- /AUTHOR -->
 
                     <!-- COMMENTS -->
