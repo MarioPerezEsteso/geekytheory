0e929736ea3c98c404e199f77ee2b2bd9f0a3e81
Mario Pérez Esteso <mario@geekytheory.com>
2016-05-22 21:28:03 +0200

    Show site menu dinamically in desktop and mobile devices

--- resources/views/themes/vortex/partials/overlay-menu.blade.php
+++ resources/views/themes/vortex/partials/overlay-menu.blade.php
@@ -1,81 +1,25 @@
 <div id="overlay-menu" class="overlay-menu">
-    <a href="index.html#" id="overlay-menu-hide" class="navigation-hide"><i class="ion-close-round"></i></a>
+    <a href="#" id="overlay-menu-hide" class="navigation-hide"><i class="ion-close-round"></i></a>
     <div class="overlay-menu-inner">
         <nav class="overlay-menu-nav">
-
             <ul id="nav">
-
-                <li class="slidedown">
-                    <a href="index.html#">Home</a>
-                    <ul>
-                        <li><a href="index.html">Light header</a></li>
-                        <li><a href="index-2.html">Dark header</a></li>
-                        <li><a href="index-3.html">Film overlay</a></li>
-                        <li><a href="index-4.html">Image Slider</a></li>
-                        <li><a href="index-5.html">Youtube Video</a></li>
-                        <li><a href="index-6.html">Text rotator</a></li>
-                        <li><a href="index-7.html">Agency</a></li>
-                    </ul>
-                </li>
-
-                <li><a href="about.html">About</a></li>
-
-                <li class="slidedown">
-                    <a href="index.html#">Portfolio</a>
-                    <ul>
-                        <li><a href="portfolio-single-1.html">Single 1 - standartd</a></li>
-                        <li><a href="portfolio-single-2.html">Single 2 - gallery</a></li>
-                        <li><a href="portfolio-single-3.html">Single 3 - full height</a></li>
-                        <li><a href="portfolio-single-4.html">Single 4 - Slider</a></li>
-                        <li><a href="portfolio-single-5.html">Single 5 - Youtube header</a></li>
-                        <li><a href="portfolio-single-6.html">Single 6 with slider</a></li>
-                        <li><a href="portfolio-single-7.html">Single 7 with Youtube</a></li>
-                        <li><a href="portfolio-single-8.html">Single 8 with Vimeo</a></li>
-                    </ul>
-                </li>
-
-                <li class="slidedown">
-                    <a href="index.html#">News</a>
-                    <ul>
-                        <li><a href="blog-grid.html">3 column grid</a></li>
-                        <li><a href="blog-grid-2.html">2 column grid</a></li>
-                        <li><a href="blog-masonry.html">Blog Masonry</a></li>
-                        <li><a href="blog-single.html">Single post</a></li>
-                    </ul>
-                </li>
-
-                <li class="slidedown">
-                    <a href="index.html#">Features</a>
-                    <ul>
-                        <li><a href="components-1.html">Buttons, Content box, Progress bars</a></li>
-                        <li><a href="components-2.html">Tab & Accordian</a></li>
-                        <li><a href="components-3.html">Pricing tables</a></li>
-                        <li><a href="components-4.html">Typography & Columns</a></li>
-                        <li><a href="components-5.html">Modules & Overlays</a></li>
-                    </ul>
-                </li>
-
-                <li><a href="contact.html">Contact</a></li>
-
+                @foreach(json_decode($siteMeta->menu, true) as $menuItem)
+                    <li>
+                        <a href="{{ $menuItem['link'] }}" title="{{ $menuItem['text'] }}">{{ $menuItem['text'] }}</a>
+                    </li>
+                @endforeach
             </ul>
-
         </nav>
     </div>
-
     <div class="overlay-navigation-footer">
-
         <div class="container">
-
             <div class="row">
-
                 <div class="col-sm-12 text-center">
-                    <p class="copyright font-alt m-b-0">© 2015 Vortex, All Rights Reserved.</p>
+                    <p class="copyright font-alt m-b-0">
+                        <!-- Copyright text -->
+                    </p>
                 </div>
-
             </div>
-
         </div>
-
     </div>
-
 </div>
