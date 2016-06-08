9f4e519cdc03242a53378d4a5941d5f2ad57f36f
Mario Pérez Esteso <mario@geekytheory.com>
2016-06-08 23:18:00 +0200

    Remove not used methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,49 +11,6 @@ use Validator;
 
 class UserController extends Controller
 {
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -97,17 +54,6 @@ class UserController extends Controller
         }
 
         return Redirect::to('home/profile/' . $user->id)->withSuccess(trans('auth.user_update_success'));
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     /**
