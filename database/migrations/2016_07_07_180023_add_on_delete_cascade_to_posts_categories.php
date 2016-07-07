ab5f61e9f5b7a8d6ab61faa27074b6cb954bc00c
Mario Pérez Esteso <mario@geekytheory.com>
2016-07-07 20:33:55 +0200

    Add migration for deleting categories on cascade with posts using FK

---
+++ database/migrations/2016_07_07_180023_add_on_delete_cascade_to_posts_categories.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOnDeleteCascadeToPostsCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts_categories', function (Blueprint $table) {
+            // Drop existent foreign keys before creating the new ones
+            $table->dropForeign(['post_id']);
+            $table->dropForeign(['category_id']);
+
+            // Foreign key to posts
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            // Foreign key to categories
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
