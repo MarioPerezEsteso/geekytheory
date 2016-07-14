4a0e16aa74a46e1426d084d3f196ad1130154584
Mario Pérez Esteso <mario@geekytheory.com>
2016-07-15 00:06:56 +0200

    Add rollback migration to posts_categories

--- database/migrations/2016_07_07_180023_add_on_delete_cascade_to_posts_categories.php
+++ database/migrations/2016_07_07_180023_add_on_delete_cascade_to_posts_categories.php
@@ -40,5 +40,22 @@ class AddOnDeleteCascadeToPostsCategories extends Migration
      */
     public function down()
     {
+        Schema::table('posts_categories', function (Blueprint $table) {
+            // Drop existent foreign keys before creating the new ones
+            $table->dropForeign(['post_id']);
+            $table->dropForeign(['category_id']);
+
+            // Foreign key to posts
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onUpdate('cascade');
+
+            // Foreign key to categories
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onUpdate('cascade');
+        });
     }
 }
