73506e9d889805be2f9c0a18fbf4d2dc1f2239fd
Mario Pérez Esteso <mario@geekytheory.com>
2016-07-25 21:40:58 +0200

    Add column published_at to posts table

---
+++ database/migrations/2016_07_25_193729_add_publishedat_field_to_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPublishedatFieldToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dateTime('published_at')->default('0000-00-00 00:00:00');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('published_at');
+        });
+    }
+}
