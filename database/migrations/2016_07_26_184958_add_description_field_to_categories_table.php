4595f87e937454cd186ec59896d9ae6ac56d6308
Mario Pérez Esteso <mario@geekytheory.com>
2016-07-26 20:58:56 +0200

    Add description field to categories table

---
+++ database/migrations/2016_07_26_184958_add_description_field_to_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDescriptionFieldToCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->text('description')->default('')->after('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+}
