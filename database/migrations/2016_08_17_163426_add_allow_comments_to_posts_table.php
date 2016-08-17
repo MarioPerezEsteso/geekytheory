d3e9ad719d653ade8d1d209fc596834f8b313855
Mario Pérez Esteso <mario@geekytheory.com>
2016-08-17 19:28:17 +0200

    Add migration to allow comments in articles

---
+++ database/migrations/2016_08_17_163426_add_allow_comments_to_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAllowCommentsToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->boolean('allow_comments')->default(true)->after('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('allow_comments');
+        });
+    }
+}
