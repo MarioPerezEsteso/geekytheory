345f3a919cff00950f9df9f2bce03334586ee9d2
Mario Pérez Esteso <mario@geekytheory.com>
2016-08-19 13:22:11 +0200

    Comment Model

---
+++ app/Comment.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'comments';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = array(
+        'post_id',
+        'user_id',
+        'parent',
+        'author_name',
+        'author_email',
+        'author_url',
+        'content',
+        'approved',
+        'spam',
+        'ip',
+    );
+
+    /**
+     * Get the user that owns the comment.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get the post that owns the comment.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
+
+}
