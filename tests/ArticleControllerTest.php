1628bffa3627cb25f052f52e5e84ec2cd8b1011e
Mario Pérez Esteso <mario@geekytheory.com>
2016-09-10 16:33:16 +0200

    Test delete exception

---
+++ tests/ArticleControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use Illuminate\Foundation\Testing\HttpException;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ArticleControllerTest extends TestCase
+{
+    use WithoutMiddleware;
+
+    /**
+     * Test delete success.
+     */
+    public function testDeleteSuccess()
+    {
+        $id = 1;
+        $this->call("GET", "home/posts/delete/$id");
+
+        $articleRepository = new \App\Repositories\ArticleRepository();
+        $article = $articleRepository->find($id);
+        $this->assertEquals(ArticleController::POST_STATUS_DELETED, $article->status);
+    }
+
+    /**
+     * Test delete post that does not exist.
+     *
+     * @expectedExceptionCode 404
+     */
+    public function testDeleteException()
+    {
+        $id = 123456;
+        $this->call("GET", "home/posts/delete/$id");
+    }
+
+}
