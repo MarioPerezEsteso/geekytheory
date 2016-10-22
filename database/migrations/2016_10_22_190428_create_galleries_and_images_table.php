7fd55d3a7d82c2db4c44e5aed9b6ac4b19a57009
Mario Pérez Esteso <mario@geekytheory.com>
2016-10-22 21:47:04 +0200

    Add migration to create the tables galleries and images

---
+++ database/migrations/2016_10_22_190428_create_galleries_and_images_table.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleriesAndImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+
+            /*
+             * Foreign key to table users to know the author of the gallery.
+             */
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade');
+        });
+
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('post_id')->unsigned();
+            $table->integer('gallery_id')->unsigned();
+            $table->string('title');
+            $table->string('image');
+            $table->enum('size', ['original', 'thumbnail', 'featured', 'featured_thumbnail']);
+            $table->integer('order');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+
+            /*
+             * Foreign key to table users to know who has uploaded the image.
+             */
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade');
+
+            /*
+             * Foreign key to table posts to know the post where the image has been uploaded.
+             */
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onUpdate('cascade');
+
+            /*
+             * Foreign key to table galleries to know the gallery where the image has been uploaded.
+             */
+            $table->foreign('gallery_id')
+                ->references('id')
+                ->on('galleries')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+        Schema::dropIfExists('galleries');
+    }
+}
