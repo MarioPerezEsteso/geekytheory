cd30c7b03aaf13798eee7b5dc66f3c0ca77cf744
Mario Pérez Esteso <mario@geekytheory.com>
2016-10-23 19:09:01 +0200

    Create Gallery repository and its interface

---
+++ app/Repositories/GalleryRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+class GalleryRepository extends Repository implements GalleryRepositoryInterface
+{
+    /**
+     * Model class name
+     *
+     * @var string
+     */
+    protected $modelClassName = 'App\Gallery';
+}
