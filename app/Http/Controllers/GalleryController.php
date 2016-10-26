01b9df849e1c60d97af726c35e4675b0baeebcf8
Mario Pérez Esteso <mario@geekytheory.com>
2016-10-26 23:28:59 +0200

    Create GalleryController

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\GalleryRepository;
+use App\User;
+use App\Validators\GalleryValidator;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class GalleryController extends Controller
+{
+    /**
+     * @var GalleryRepository
+     */
+    protected $galleryRepository;
+
+    /**
+     * @var GalleryValidator
+     */
+    protected $galleryValidator;
+
+    /**
+     * GalleryController constructor.
+     *
+     * @param GalleryRepository $galleryRepository
+     * @param GalleryValidator $galleryValidator
+     */
+    public function __construct(GalleryRepository $galleryRepository, GalleryValidator $galleryValidator)
+    {
+        $this->galleryRepository = $galleryRepository;
+        $this->galleryValidator = $galleryValidator;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = [
+            'user_id' => null,
+            'title' => $request->title,
+        ];
+
+        /** @var User */
+        $user = Auth::user();
+        if ($user !== null) {
+            $data['user_id'] = $user->getAttribute('id');
+        }
+
+        if (!$this->galleryValidator->with($data)->passes()) {
+            return Redirect::to('home/gallery/create')->withErrors($this->galleryValidator->errors());
+        } else {
+            $gallery = $this->galleryRepository->create($data);
+
+            return Redirect::to('home/gallery/edit/' . $gallery->id)->withSuccess(trans('home.gallery_create_success'));
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
