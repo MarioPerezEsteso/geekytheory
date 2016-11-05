9130e4c58eca3279c19c8afa6f3301747ad65bfe
Mario Pérez Esteso <mario@geekytheory.com>
2016-11-05 13:02:13 +0100

    Add method to find galleries by their author

--- app/Repositories/GalleryRepository.php
+++ app/Repositories/GalleryRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repositories;
 
+use App\User;
+
 class GalleryRepository extends Repository implements GalleryRepositoryInterface
 {
     /**
@@ -10,4 +12,26 @@ class GalleryRepository extends Repository implements GalleryRepositoryInterface
      * @var string
      */
     protected $modelClassName = 'App\Gallery';
+
+    /**
+     * Find galleries owned by an author or all galleries joined with their authors.
+     *
+     * @param User|null $author
+     * @param int $paginate
+     * @return mixed
+     */
+    public function findGalleries(User $author = null, $paginate = Repository::PAGINATION_DEFAULT)
+    {
+        $columns = User::$mappings;
+        array_push($columns, 'galleries.*');
+        $query = call_user_func_array("{$this->modelClassName}::join", array('users', 'users.id', '=', 'galleries.user_id'))
+            ->select($columns)
+            ->orderBy('galleries.created_at', 'DESC');
+
+        if ($author !== null) {
+            $query->where('users.username', $author->username);
+        }
+
+        return $query->paginate($paginate);
+    }
 }
--- app/Repositories/GalleryRepositoryInterface.php
+++ app/Repositories/GalleryRepositoryInterface.php
@@ -2,6 +2,9 @@
 
 namespace App\Repositories;
 
+use App\User;
+
 interface GalleryRepositoryInterface
 {
+    public function findGalleries(User $author = null, $paginate = Repository::PAGINATION_DEFAULT);
 }
