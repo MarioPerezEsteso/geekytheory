ab7ac81cff535855c2a8d9eb6da518e53de2d6da
Mario Pérez Esteso <mario@geekytheory.com>
2016-11-09 13:30:42 +0100

    Test method findImagesByGallery in images repository

--- tests/ImageRepositoryTest.php
+++ tests/ImageRepositoryTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Image;
+use App\Repositories\GalleryRepository;
 use App\Repositories\ImageRepository;
 
 class ImageRepositoryTest extends TestCase
@@ -10,10 +11,36 @@ class ImageRepositoryTest extends TestCase
      */
     protected $imageRepository;
 
+    /**
+     * @var GalleryRepository
+     */
+    protected $galleryRepository;
+
     public function setUp()
     {
         parent::setUp();
         $this->imageRepository = new ImageRepository();
+        $this->galleryRepository = new GalleryRepository();
+    }
+
+    /**
+     * Test findImagesByGallery searching all of the images with different sizes.
+     */
+    public function testFindImagesByGalleryInAllSizes()
+    {
+        $gallery = $this->galleryRepository->find(1);
+        $images = $this->imageRepository->findImagesByGallery($gallery);
+        $this->assertEquals(10, count($images));
+    }
+
+    /**
+     * Test findImagesByGallery searching all of the images with original size.
+     */
+    public function testFindImagesByGalleryWithOriginalSize()
+    {
+        $gallery = $this->galleryRepository->find(1);
+        $images = $this->imageRepository->findImagesByGallery($gallery, Image::SIZE_ORIGINAL);
+        $this->assertEquals(5, count($images));
     }
 
     /**
