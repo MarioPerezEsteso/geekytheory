54edc2227b40125bb203fe568229769dba25b023
Mario Pérez Esteso <mario@geekytheory.com>
2016-12-18 20:58:04 +0100

    Test social share counter updates

--- tests/ArticleControllerTest.php
+++ tests/ArticleControllerTest.php
@@ -31,4 +31,51 @@ class ArticleControllerTest extends TestCase
         $this->call("GET", "home/posts/delete/$id");
     }
 
+	/**
+	 * Test update share count of article success.
+	 */
+	public function testUpdateSharesSuccess()
+	{
+		$postId = 1;
+		$data = [
+			'postId' => $postId,
+			'socialNetwork' => 'whatsapp',
+		];
+		$this->call('POST', 'share-article', $data);
+
+		$this->assertResponseOk();
+
+		$post = (new \App\Repositories\ArticleRepository())->find($postId);
+		$this->assertEquals(1, $post->shares_whatsapp);
+	}
+
+	/**
+	 * Test update share count of an article that does not exist.
+	 *
+	 * @expectedExceptionCode 404
+	 */
+	public function testUpdateSharesOfUndefinedPost()
+	{
+		$postId = 1322112;
+		$data = [
+			'postId' => $postId,
+			'socialNetwork' => 'whatsapp',
+		];
+		$this->call('POST', 'share-article', $data);
+	}
+
+	/**
+	 * Test update share count of an article with a social network that does not exist.
+	 */
+	public function testUpdateSharesOfUndefinedSocialNetwork()
+	{
+		$postId = 1;
+		$data = [
+			'postId' => $postId,
+			'socialNetwork' => 'invented_social_network',
+		];
+		$result = $this->call('POST', 'share-article', $data);
+
+		$this->assertEquals('1', $result->original['error']);
+	}
 }
