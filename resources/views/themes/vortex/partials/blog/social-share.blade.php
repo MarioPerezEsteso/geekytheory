9384bb41551252fac8a907001504d3ea41139f12
Mario Pérez Esteso <mario@geekytheory.com>
2016-12-18 19:49:24 +0100

    Show whatsapp share icon only in mobile devices

--- resources/views/themes/vortex/partials/blog/social-share.blade.php
+++ resources/views/themes/vortex/partials/blog/social-share.blade.php
@@ -2,7 +2,10 @@
     <ul class="social-icon-links socicon-round">
         @foreach($socialShareButtons as $socialShareButton)
             <li>
-                <a href="{{ $socialShareButton['url'] }}" title="{{ $socialShareButton['title'] }}" data-post-id="{{ $post->id }}" data-social-network="{{ $socialShareButton['socialNetwork'] }}" class="social-icon-link {{ $socialShareButton['socialNetwork'] }}" target="_blank">
+                <?php $onlyVisibleMobile = !$socialShareButton['visibleDesktop'] && $socialShareButton['visibleMobile']; ?>
+                <a href="{{ $socialShareButton['url'] }}" title="{{ $socialShareButton['title'] }}"
+                   data-post-id="{{ $post->id }}" data-social-network="{{ $socialShareButton['socialNetwork'] }}"
+                   class="social-icon-link {{ $socialShareButton['socialNetwork'] }} {{ $onlyVisibleMobile ? 'visible-xs' : '' }}" target="_blank">
                     <i class="fa {{ $socialShareButton['icon'] }}"></i>
                 </a>
             </li>
