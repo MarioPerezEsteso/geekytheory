4e4531769b01295333c1754dd0b7e89db82c11de
Mario Pérez Esteso <mario@geekytheory.com>
2017-01-06 16:48:24 +0000

    Send email notifications when a new comment arrives

---
+++ resources/lang/en/email.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Email Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'someone_answered_your_comment_in' => 'Someone answered your comment in :sitetitle',
+    'new_answer_to_your_comment' => 'New answer to your comment - :sitetitle',
+	'thankyou_for_reading_us' => 'Thank you for reading us.',
+	'see_the_answer' => 'See the answer',
+	'hi_name_this_was_your_comment' => 'Hello :name, this was your comment:'
+];
