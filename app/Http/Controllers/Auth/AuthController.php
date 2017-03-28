cbc70cf59542dce69e3c0bd76d0c1c1a8385b6bd
Mario Pérez Esteso <mario@geekytheory.com>
2017-03-28 23:39:27 +0200

    feat(postsUsers): add column to forbid user login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
@@ -75,4 +77,48 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+
+	/**
+	 * Handle a login request to the application.
+	 *
+	 * @param Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function postLogin(Request $request)
+	{
+		$this->validate($request, [
+			$this->loginUsername() => 'required', 'password' => 'required',
+		]);
+
+		// If the class is using the ThrottlesLogins trait, we can automatically throttle
+		// the login attempts for this application. We'll key this by the username and
+		// the IP address of the client making these requests into this application.
+		$throttles = $this->isUsingThrottlesLoginsTrait();
+
+		if ($throttles && $this->hasTooManyLoginAttempts($request)) {
+			return $this->sendLockoutResponse($request);
+		}
+
+		$credentials = $this->getCredentials($request);
+		// Check if the user can login or not
+		$credentials['can_login'] = 1;
+
+		if (Auth::attempt($credentials, $request->has('remember'))) {
+			return $this->handleUserWasAuthenticated($request, $throttles);
+		}
+
+		// If the login attempt was unsuccessful we will increment the number of attempts
+		// to login and redirect the user back to the login form. Of course, when this
+		// user surpasses their maximum number of attempts they will get locked out.
+		if ($throttles) {
+			$this->incrementLoginAttempts($request);
+		}
+
+		return redirect($this->loginPath())
+			->withInput($request->only($this->loginUsername(), 'remember'))
+			->withErrors([
+				$this->loginUsername() => $this->getFailedLoginMessage(),
+			]);
+	}
+
 }
