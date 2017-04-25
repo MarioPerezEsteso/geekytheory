3c6020f20601dde130d207462bcfa905e4d3ac41
Mario Pérez Esteso <mario@geekytheory.com>
2017-04-25 21:11:23 +0200

    feat(postUsers): add rules for UserMetaValidator

--- app/Validators/UserMetaValidator.php
+++ app/Validators/UserMetaValidator.php
@@ -11,6 +11,20 @@ class UserMetaValidator extends LaravelValidator implements ValidableInterface
      * Validation rules for creating the metadata of a User.
      */
     protected $rules = [
+        'biography' => 'max:255',
+		'job' => 'max:255',
+		'twitter' => 'url|max:255',
+		'instagram' => 'url|max:255',
+		'facebook' => 'url|max:255',
+		'github' => 'url|max:255',
+		'youtube' => 'url|max:255',
+		'googleplus' => 'url|max:255',
+		'stackoverflow' => 'url|max:255',
+		'bitbucket' => 'url|max:255',
+		'linkedin' => 'url|max:255',
+		'tumblr' => 'url|max:255',
+		'twitch' => 'url|max:255',
+		'vimeo' => 'url|max:255',
 	];
 
     /**
