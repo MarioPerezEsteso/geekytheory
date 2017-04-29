6bcd39140bfddabe1064d133cf574b0d03fec108
Mario Pérez Esteso <mario@geekytheory.com>
2017-04-29 16:17:01 +0200

    feat(postsUsers): test user validation update

--- tests/unit/Validators/UserValidatorTest.php
+++ tests/unit/Validators/UserValidatorTest.php
@@ -5,6 +5,11 @@ use \App\Validators\UserValidator;
 class UserValidatorTest extends TestCase
 {
     /**
+     * @return array
+     */
+    protected $users;
+
+    /**
      * Set up test
      *
      * @return void
@@ -12,14 +17,16 @@ class UserValidatorTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+
         // Create two users
-        factory(App\User::class)->create([
+        $this->users[0] = factory(App\User::class)->create([
             'name' => 'Alice',
             'email' => 'alice@geekytheory.com',
             'password' => bcrypt('123456'),
             'username' => 'alice',
-        ],
-        [
+        ]);
+
+        $this->users[1] = factory(App\User::class)->create([
             'name' => 'Bob',
             'email' => 'bob@geekytheory.com',
             'password' => bcrypt('123456'),
@@ -31,12 +38,61 @@ class UserValidatorTest extends TestCase
      * Test create with valid data.
      * 
      * @dataProvider getValidCreateData
+     * @param array $data
      */
     public function testValidationOk($data)
     {
         $validator = new UserValidator(App::make('validator'));
         $passes = $validator->with($data)->passes();
         $this->assertTrue($passes);
+    }
+
+    /**
+     * Test create with valid data.
+     * 
+     * @dataProvider getInvalidCreateData
+     * @param array $data
+     */
+    public function testValidationError($data, $validationErrorKeys)
+    {
+        $validator = new UserValidator(App::make('validator'));
+        $passes = $validator->with($data)->passes();
+        $this->assertFalse($passes);
+        $this->assertEquals(count($validationErrorKeys), count($validator->errors()));
+        foreach ($validationErrorKeys as $validationErrorKey) {
+            $this->assertArrayHasKey($validationErrorKey, $validator->errors()->toArray());
+        }
+    }
+
+    /**
+     * Test update with valid data.
+     *
+     * @dataProvider getValidUpdateData
+     * @param array $data
+     */
+    public function testUpdateValidationOk($data)
+    {
+        $validator = new UserValidator(App::make('validator'));
+        $passes = $validator->with($data)->update($this->users[0]->id)->passes();
+        $this->assertTrue($passes);
+    }
+
+    /**
+     * Test update with invalid data.
+     * 
+     * @dataProvider getInvalidUpdateData
+     * @param array $data
+     * @param array $data
+     */
+    public function testUpdateValidationError($data, $validationErrorKeys)
+    {
+        $validator = new UserValidator(App::make('validator'));
+        $passes = $validator->with($data)->update($this->users[0]->id)->passes();
+        $this->assertFalse($passes);
+        $this->assertEquals(count($validationErrorKeys), count($validator->errors()));
+        foreach ($validationErrorKeys as $validationErrorKey) {
+            $this->assertArrayHasKey($validationErrorKey, $validator->errors()->toArray());
+        }
     }
 
     /**
@@ -72,19 +128,35 @@ class UserValidatorTest extends TestCase
     }
 
     /**
-     * Test create with valid data.
+     * Returns valid update data.
      * 
-     * @dataProvider getInvalidCreateData
+     * @return array
      */
-    public function testValidationError($data, $validationErrorKeys)
+    public static function getValidUpdateData()
     {
-        $validator = new UserValidator(App::make('validator'));
-        $passes = $validator->with($data)->passes();
-        $this->assertFalse($passes);
-        $this->assertEquals(count($validationErrorKeys), count($validator->errors()));
-        foreach ($validationErrorKeys as $validationErrorKey) {
-            $this->assertArrayHasKey($validationErrorKey, $validator->errors()->toArray());
-        }
+        return [
+            [ // Test input 1
+                [
+                    'name' => 'Alice Pérez',
+                    'email' => 'alice@geekytheory.com',
+                    'username' => 'alice',
+                ],
+            ],
+            [ // Test input 2
+                [
+                    'name' => 'Alice',
+                    'email' => 'alice@geekytheory.com',
+                    'username' => 'mario010203'
+                ],
+            ],
+            [ // Test input 3
+                [
+                    'name' => 'Alice',
+                    'email' => 'alice2@geekytheory.com',
+                    'username' => 'alice'
+                ],
+            ],
+        ];
     }
 
     /**
@@ -132,6 +204,55 @@ class UserValidatorTest extends TestCase
                     'username' => 'a'
                 ],
                 'validationErrorKeys' => ['name', 'email', 'username'],
+            ],
+        ];
+    }
+
+    /**
+     * Returns invalid data.
+     * 
+     * @return array
+     */
+    public static function getInvalidUpdateData()
+    {
+        return [
+            [ // Test input 1
+                [
+                    'name' => 'Alice',
+                    'email' => 'alice@geekytheory.com',
+                ],
+                'validationErrorKeys' => ['username'],
+            ],
+            [ // Test input 2
+                [
+                    'name' => 'Mario',
+                    'email' => 'mario@geekytheory.com',
+                    'username' => 'mario perez'
+                ],
+                'validationErrorKeys' => ['username']
+            ],
+            [ // Test input 3
+                [
+                    'name' => 'Mario',
+                    'email' => 'mario@geekytheory.com',
+                    'username' => 'mario_perez'
+                ],
+                'validationErrorKeys' => ['username']
+            ],
+            [ // Test input 4
+                [
+                    'name' => 'Alice',
+                    'email' => 'bob@geekytheory.com',
+                    'username' => 'alice2'
+                ],
+                'validationErrorKeys' => ['email']
+            ],
+            [ // Test input 5
+                [
+                    'email' => 'alice@geekytheory.com',
+                    'username' => 'a'
+                ],
+                'validationErrorKeys' => ['name', 'username']
             ],
         ];
     }
