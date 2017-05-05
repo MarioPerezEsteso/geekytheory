ba41d593124b837599d2862d4e687f1be1301f71
Mario Pérez Esteso <mario@geekytheory.com>
2017-05-05 15:30:46 +0200

    feat(travisCI): setup travis CI

--- config/database.php
+++ config/database.php
@@ -85,6 +85,18 @@ return [
             'prefix'   => '',
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'db_test'),
+            'username'  => env('DB_USERNAME', 'homestead'),
+            'password'  => env('DB_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
     ],
 
     /*
