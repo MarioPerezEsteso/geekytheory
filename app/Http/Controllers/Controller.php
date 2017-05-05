a6ec354382a2efb6a0de45a9e777b831aa5f131d
Mario Pérez Esteso <mario@geekytheory.com>
2017-05-06 00:28:34 +0200

    feat(laravelUpdate): install Laravel 5.4

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,34 +7,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-abstract class Controller extends BaseController
+class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Available social networks
-     *
-     * @var array
-     */
-    public static $socialNetworks = array(
-        'twitter',
-        'instagram',
-        'facebook',
-        'linkedin',
-        'github',
-        'youtube',
-        'dribbble',
-        'google-plus',
-        'stack-overflow',
-        'flickr',
-        'bitbucket'
-    );
-
-    /**
-     * URL regex validator
-     *
-     * @var string
-     */
-    public static $urlRegexValidator = 'regex:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
-
 }
--- app/User.php
+++ app/User.php
@@ -2,94 +2,28 @@
 
 namespace App;
 
-use App\Http\Controllers\UserController;
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model implements AuthenticatableContract,
-    AuthorizableContract,
-    CanResetPasswordContract
+class User extends Authenticatable
 {
-    use Authenticatable, Authorizable, CanResetPassword;
-
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = array(
-        'name',
-        'username',
-        'email',
-        'password',
-    );    
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 
     /**
-     * The attributes excluded from the model's JSON form.
+     * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token', 'can_login'];
-
-    /**
-     * This array is used when a query joins with this table
-     * and the columns need to be aliased.
-     *
-     * @var array
-     */
-    public static $mappings = array(
-        'users.id as user_id',
-        'users.name as user_name',
-        'users.username as user_username',
-        'users.email as user_email',
-        'users.password as user_password',
-    );
-
-    /**
-     * Get the posts of the user.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function posts()
-    {
-        return $this->hasMany('App\Post');
-    }
-
-    /**
-     * Get the user meta.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function userMeta()
-	{
-		return $this->hasOne('App\UserMeta');
-	}
-
-    /**
-     * Returns the basic user data for the admin panel view
-     *
-     * @return array
-     */
-    public function getBasicUserData()
-    {
-        return array(
-            'id' => $this->id,
-            'name' => $this->name,
-            'job' => $this->job,
-            'email' => $this->email,
-            'avatar' => getGravatar($this->email, '100', 'mm', 'g'));
-    }
-
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
