3d21a700147dee8161d5e34b5b8017a77189374f
Mario Pérez Esteso <mario@geekytheory.com>
2017-05-06 16:59:57 +0200

    feat(emailComments): refactor comment validator and its tests

--- tests/Unit/Validators/CommentValidatorTest.php
+++ tests/Unit/Validators/CommentValidatorTest.php
@@ -6,53 +6,124 @@ use App\Validators\CommentValidator;
 class CommentValidatorTest extends TestCase
 {
     /**
-     * Test valid data.
+     * Test create with valid data.
+     *
+     * @dataProvider getValidCreateData
+     * @param array $data
      */
-    public function testCreateSuccess()
+    public function testCreateSuccess($data)
     {
         $validator = new CommentValidator(App::make('validator'));
-        $this->assertTrue($validator->with($this->getValidCreateData())->passes());
+        $this->assertTrue($validator->with($data)->passes());
     }
 
     /**
      * Test invalid data.
+     * @dataProvider getInvalidCreateData
+     * @param array $data
      */
-    public function testCreateFailure()
+    public function testCreateFailure($data, $validationErrorKeys)
     {
         $validator = new CommentValidator(App::make('validator'));
-        $this->assertFalse($validator->with($this->getInvalidCreateData())->passes());
-        $this->assertEquals(2, count($validator->errors()));
+        $this->assertFalse($validator->with($data)->passes());
+        $this->assertEquals(count($validationErrorKeys), count($validator->errors()));
         $this->assertInstanceOf('Illuminate\Support\MessageBag', $validator->errors());
+        foreach ($validationErrorKeys as $validationErrorKey) {
+            $this->assertArrayHasKey($validationErrorKey, $validator->errors()->toArray());
+        }
     }
 
     /**
      * Returns an array with an example of valid data.
      */
-    private function getValidCreateData()
+    public static function getValidCreateData()
     {
-        return array(
-            'post_id' => 1,
-            'author_name' => 'John Doe',
-            'author_email' => 'mario@domain.com',
-            'author_url' => 'http://geekytheory.com',
-            'body' => 'This is the comment body!',
-            'approved' => true,
-            'spam' => false,
-        );
+        return [
+            [
+                [
+                    'post_id' => 1,
+                    'author_name' => 'John Doe',
+                    'author_email' => 'mario@domain.com',
+                    'author_url' => 'http://geekytheory.com',
+                    'body' => 'This is the comment body!',
+                    'approved' => true,
+                    'spam' => false,
+                ]
+            ],
+            [
+                [
+                    'post_id' => 1,
+                    'author_name' => 'Alice McDonalds',
+                    'author_email' => 'alice@mc.com',
+                    'body' => 'This is the comment body!',
+                    'approved' => true,
+                    'spam' => false,
+                ],
+            ],
+            [
+                [
+                    'post_id' => 1,
+                    'author_name' => 'Alice McDonalds',
+                    'author_email' => 'alice@mc.com',
+                    'author_url' => '',
+                    'body' => 'This is the comment body!',
+                    'approved' => true,
+                    'spam' => false,
+                ]
+            ],
+        ];
     }
 
     /**
      * Returns an array with an example of invalid data.
-     * Author email ans comment body are missing.
      */
-    private function getInvalidCreateData()
+    public static function getInvalidCreateData()
     {
-        return array(
-            'post_id' => 1,
-            'author_name' => 'John Doe',
-            'author_url' => 'http://geekytheory.com',
-            'approved' => true,
-            'spam' => false,
-        );
+        return [
+            [
+                [
+                    'author_name' => 'John Doe',
+                    'author_email' => 'mario@domain.com',
+                    'author_url' => 'http://geekytheory.com',
+                    'approved' => true,
+                    'spam' => false,
+                ],
+                'validationErrorKeys' => ['post_id', 'body'],
+            ],
+            [
+                [
+                    'post_id' => 1,
+                    'author_name' => 'Alice McDonalds',
+                    'body' => 'This is the comment body!',
+                    'approved' => true,
+                    'spam' => false,
+                ],
+                'validationErrorKeys' => ['author_email'],
+            ],
+            [
+                [
+                    'post_id' => 1,
+                    'author_name' => 'Alice McDonalds',
+                    'author_email' => 'alice@mc.com',
+                    'author_url' => 'invalid url',
+                    'body' => 'This is the comment body!',
+                    'approved' => true,
+                    'spam' => false,
+                ],
+                'validationErrorKeys' => ['author_url'],
+            ],
+            [
+                [
+                    'post_id' => 1,
+                    'author_name' => 'Alice McDonalds',
+                    'author_email' => 'alice@mc.com',
+                    'author_url' => 'http://geekytheory.com',
+                    'body' => 'This is the comment body!',
+                    'approved' => 'yes',
+                    'spam' => 'no',
+                ],
+                'validationErrorKeys' => ['approved', 'spam'],
+            ]
+        ];
     }
 }
