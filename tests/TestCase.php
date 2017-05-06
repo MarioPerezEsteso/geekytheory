441685184d0a9592b1fe93342dd44cdb18155719
Mario Pérez Esteso <mario@geekytheory.com>
2017-05-06 11:52:52 +0200

    feat(laravelUpdate): remove laravel 5.4 tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,40 @@
 <?php
 
-namespace Tests;
-
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase
+class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-    use CreatesApplication;
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->createApplication();
+        Artisan::call('migrate:reset');
+        Artisan::call('migrate');
+        Artisan::call('db:seed', array("--class" => "TestDatabaseSeeder"));
+    }
+
+    public function tearDown()
+    {
+        Artisan::call('migrate:reset');
+        parent::tearDown();
+    }
 }
