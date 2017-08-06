bde80529a51e824e8fb341691aeb8a4bb8d5de7a
Mario Pérez Esteso <mario@geekytheory.com>
2017-08-06 21:58:43 +0200

    feat(university): integrate Dingo and Laravel Passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -27,5 +27,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        app('Dingo\Api\Auth\Auth')->extend('passport', function ($app) {
+            return app(PassportAuthenticationServiceProvider::class);
+        });
     }
 }
---
+++ config/passport.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'client' => [
+        'id' => env('PASSPORT_CLIENT_ID', null),
+        'secret' => env('PASSPORT_CLIENT_SECRET', null),
+    ]
+];
