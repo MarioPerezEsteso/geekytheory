1e179233497771c9a18ba3853043c699c6662daa
Mario Pérez Esteso <mario@geekytheory.com>
2017-08-09 20:54:52 +0200

    feat(university): create TestConstants class

---
+++ tests/Helpers/TestConstants.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Helpers;
+
+class TestConstants
+{
+    /**
+     * Model resource types.
+     */
+    const RESOURCE_TYPE_COURSE = 'Course';
+
+    /********************
+     * Model constants. *
+     ********************/
+    /**
+     * Course.php
+     */
+    const MODEL_COURSE_DIFFICULTY_BEGINNER = 'beginner';
+    const MODEL_COURSE_DIFFICULTY_INTERMEDIATE = 'intermediate';
+    const MODEL_COURSE_DIFFICULTY_ADVANCED = 'advanced';
+}
