4656b31da28eefbf310f9c10e34aae025b0e296b
Mario Pérez Esteso <mario@geekytheory.com>
2017-08-18 17:56:57 +0000

    feat(universit): add includes to test endpoint creation

--- tests/Helpers/TestUtils.php
+++ tests/Helpers/TestUtils.php
@@ -9,13 +9,17 @@ class TestUtils
      *
      * @param string $endpoint
      * @param array $params
-     *
+     * @param array $includes
      * @return string
      */
-    public static function createEndpoint($endpoint, $params = []): string
+    public static function createEndpoint($endpoint, $params = [], $includes = []): string
     {
         foreach ($params as $param => $value) {
             $endpoint = str_replace('{' . $param . '}', $value, $endpoint);
+        }
+
+        if (count($includes) > 0) {
+            $endpoint .= '?include=' . implode(',', $includes);
         }
 
         return $endpoint;
