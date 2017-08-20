eba2f4ed8ac2eadbeec352d47b85c3f3e9ed1f79
Mario Pérez Esteso <mario@geekytheory.com>
2017-08-20 21:19:36 +0200

    feat(university): create CourseController

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Course;
+use App\Exceptions\ExceptionFactory;
+
+class CourseController extends Controller
+{
+    /**
+     * Show single course.
+     *
+     * @param $slug
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function show($slug)
+    {
+        $course = Course::getPublished(['slug' => $slug])->with('teacher')->firstOrFail();
+
+        return view('courses.course', compact('course'));
+    }
+}
