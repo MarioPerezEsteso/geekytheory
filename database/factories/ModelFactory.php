83458f18242b3b79d38b867180c65c64b07def70
Mario Pérez Esteso <mario@geekytheory.com>
2017-08-23 19:23:02 +0200

    feat(university): create Courses seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,6 +15,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
+        'username' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
