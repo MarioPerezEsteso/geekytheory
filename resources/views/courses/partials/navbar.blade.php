eae3dcd996e7499a861cf1dbcf4d9bc0bc3e99bb
Mario Pérez Esteso <mario@geekytheory.com>
2017-08-27 13:27:05 +0200

    feat(university): make navbar dynamic

--- resources/views/courses/partials/navbar.blade.php
+++ resources/views/courses/partials/navbar.blade.php
@@ -1,41 +1,3 @@
-{{--<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
-                <span class="sr-only">Toggle navigation</span>
-            </button>
-            <a class="navbar-brand" href="#">GT</a>
-        </div>
-        <div class="collapse navbar-collapse" id="navbar-collapse-01">
-            <form class="navbar-form navbar-right" action="#" role="search">
-                <div class="form-group">
-                    <div class="input-group">
-                        <input class="form-control" id="navbarInput-01" type="search" placeholder="Buscar">
-                        <span class="input-group-btn">
-                      <button type="submit" class="btn"><span class="fui-search"></span></button>
-                    </span>
-                    </div>
-                </div>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cursos <b class="caret"></b></a>
-                    <span class="dropdown-arrow"></span>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">item 1</a></li>
-                        <li><a href="#">item 2</a></li>
-                        <li><a href="#">item 3</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">item </a></li>
-                    </ul>
-                </li>
-                <li><a href="#fakelink">Blog</a></li>
-                <li><a href="#fakelink">Acceder</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div>
-</nav><!-- /navbar -->--}}
-
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
@@ -51,22 +13,30 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Courses</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">About</a></li>
+                @foreach(json_decode($siteMeta->menu, true) as $menuItem)
+                    <?php $hasSubmenu = ($menuItem['submenu'] !== null); ?>
+                    @if (!$hasSubmenu)
+                        <li>
+                            <a href="{{ $menuItem['link'] }}"
+                               title="{{ $menuItem['text'] }}">{{ $menuItem['text'] }}</a>
+                        </li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-expanded="false">
+                                {{ $menuItem['text'] }}<span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                @foreach($menuItem['submenu'] as $submenuItem)
+                                    <li>
+                                        <a href="{{ $submenuItem['link'] }}"
+                                           title="{{ $submenuItem['text'] }}">{{ $submenuItem['text'] }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+                @endforeach
             </ul>
         </div>
         <!--/.nav-collapse -->
