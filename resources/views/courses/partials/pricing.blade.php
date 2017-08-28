56fdd1df07711c5f1a8742f6c665b61f7c9f05c6
Mario Pérez Esteso <mario@geekytheory.com>
2017-08-28 20:54:22 +0200

    feat(university): create partial for pricing

---
+++ resources/views/courses/partials/pricing.blade.php
@@ -0,0 +1,64 @@
+<div class="row pricing">
+    <div class="col-sm-8 col-sm-offset-2 col-xs-12">
+        <h2>Planes</h2>
+        <br>
+        <div class="row">
+            <div class="col-md-offset-2 col-md-4 col-sm-offset-1 col-sm-5 col-xs-12">
+                <ul class="plan plan1">
+                    <li class="plan-name">
+                        Básico
+                    </li>
+                    <li class="plan-price">
+                        <strong>Gratis</strong>
+                    </li>
+                    <li>
+                        <strong>5GB</strong> Storage
+                    </li>
+                    <li>
+                        <strong>1GB</strong> RAM
+                    </li>
+                    <li>
+                        <strong>400GB</strong> Bandwidth
+                    </li>
+                    <li>
+                        <strong>10</strong> Email Address
+                    </li>
+                    <li>
+                        <strong>Forum</strong> Support
+                    </li>
+                    <li class="plan-action">
+                        <a href="#" class="btn btn-inverse btn-lg">Signup</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 col-sm-5 col-xs-12">
+                <ul class="plan plan2 featured">
+                    <li class="plan-name">
+                        Premium
+                    </li>
+                    <li class="plan-price">
+                        <strong>15 €</strong> / mes
+                    </li>
+                    <li>
+                        <strong>5GB</strong> Storage
+                    </li>
+                    <li>
+                        <strong>1GB</strong> RAM
+                    </li>
+                    <li>
+                        <strong>400GB</strong> Bandwidth
+                    </li>
+                    <li>
+                        <strong>10</strong> Email Address
+                    </li>
+                    <li>
+                        <strong>Forum</strong> Support
+                    </li>
+                    <li class="plan-action">
+                        <a href="#" class="btn btn-primary btn-lg">Signup</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
