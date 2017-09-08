f3b47d17978588ff8e702da6d1b4695bc8d6bf4f
Mario Pérez Esteso <mario@geekytheory.com>
2017-09-08 16:26:47 +0000

    feat(university): move lesson video HTML to partial

--- resources/views/courses/lesson.blade.php
+++ resources/views/courses/lesson.blade.php
@@ -29,17 +29,7 @@
 
 @include('courses.partials.navbar')
 
-<div class="jumbotron jumbotron-post">
-    <div class="container container-post">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="embed-responsive embed-responsive-16by9">
-                    {!! $lesson->video !!}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@include('courses.partials.lesson.video')
 
 <section class="container">
     <div class="row">
---
+++ resources/views/courses/partials/lesson/video.blade.php
@@ -0,0 +1,11 @@
+<div class="jumbotron jumbotron-post">
+    <div class="container container-post">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    {!! $lesson->video !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
