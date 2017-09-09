fd3c01f4ed44a7d73db8b2712da77e6031eb26a4
Mario Pérez Esteso <mario@geekytheory.com>
2017-09-09 10:57:49 +0000

    feat(university): create view for registration in lesson

---
+++ resources/views/courses/partials/lesson/headerRegister.blade.php
@@ -0,0 +1,40 @@
+<div class="jumbotron jumbotron-home center-flex gradient-blue-geeky">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h1 class="jumbotron-title">Regístrate para ver este curso</h1>
+                <p>Este curso es gratuito y podrás verlo siempre que quieras una vez hayas creado tu cuenta.</p>
+            </div>
+            <div class="col-lg-5 col-lg-push-1">
+                <div class="login-jumbotron">
+                    <div class="login-jumbotron-trial-banner">
+                        <p>Pruébalo gratis durante 7 días</p>
+                    </div>
+                    <form class="login-form">
+                        <div class="form-group">
+                            <input type="text" class="form-control login-field" value=""
+                                   placeholder="Introduce tu nombre"
+                                   id="login-name">
+                            <label class="login-field-icon fui-user" for="login-name"></label>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" class="form-control login-field" value=""
+                                   placeholder="Introduce tu email"
+                                   id="login-name">
+                            <label class="login-field-icon fui-mail" for="login-name"></label>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" class="form-control login-field" value="" placeholder="Contraseña"
+                                   id="login-pass">
+                            <label class="login-field-icon fui-lock" for="login-pass"></label>
+                        </div>
+
+                        <a class="btn btn-primary btn-lg btn-block" href="#">Obtener 7 días gratis</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
