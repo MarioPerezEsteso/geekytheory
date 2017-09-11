624d2680144172bda8aac2e616e3543b8c81b146
Mario Pérez Esteso <mario@geekytheory.com>
2017-09-11 13:34:02 +0200

    test(register): register two users with same name creates two usernames

--- tests/Functional/RegisterControllerTest.php
+++ tests/Functional/RegisterControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Functional;
 
+use App\User;
 use Tests\TestCase;
 
 class RegisterControllerTest extends TestCase
@@ -47,6 +48,47 @@ class RegisterControllerTest extends TestCase
             'name' => $registrationData['name'],
             'email' => $registrationData['email'],
             'username' => formatNameToUsername($registrationData['name']),
+        ]);
+    }
+
+    /**
+     * Test register user with the same name of another user already registered user creates a username
+     * with a number as suffix.
+     */
+    public function testRegisterUserWithAlreadyExistentNameCreatesUsernameWithSuffix()
+    {
+        // Config
+        $faker = \Faker\Factory::create();
+        $userData = [
+            'name' => $faker->name,
+            'email' => $faker->email,
+            'password' => $faker->password(6),
+        ];
+        $userData['username'] = formatNameToUsername($userData['name']);
+
+        // Prepare
+        factory(User::class)->create($userData);
+
+        // Register user with the same name.
+        $registrationData = [
+            'name' => $userData['name'],
+            'email' => $faker->email,
+            'password' => $faker->password(6),
+        ];
+
+        // Request
+        $response = $this->call('POST', $this->registerUrl, $registrationData);
+
+        // Asserts
+        $response->assertStatus(302);
+
+        $response->assertHeader('location', $this->urlToRedirectOnRegister);
+
+        // We append a suffix to username because it was already in the database
+        $this->assertDatabaseHas('users', [
+            'name' => $registrationData['name'],
+            'email' => $registrationData['email'],
+            'username' => formatNameToUsername($registrationData['name']) . '1',
         ]);
     }
 
