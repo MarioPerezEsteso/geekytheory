87de089e5bb50adb5b4e08022baf7893d789dcb9
Mario Pérez Esteso <mario@geekytheory.com>
2017-09-11 12:01:13 +0200

    feat(tests): add trait CreatesApplication from Laravel 5.4

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,8 @@ use Tests\Helpers\Response;
 
 class TestCase extends \Illuminate\Foundation\Testing\TestCase
 {
+    use CreatesApplication;
+
     /**
      * The base URL to use while testing the application.
      *
@@ -22,20 +24,6 @@ class TestCase extends \Illuminate\Foundation\Testing\TestCase
      * @var array
      */
     protected $server = [];
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__ . '/../bootstrap/app.php';
-
-        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
 
     public function setUp()
     {
