1521a10daefdb5a546c3d5580bb2ce34395bfa8d
Mario Pérez Esteso <mario@geekytheory.com>
2017-09-12 16:27:53 +0000

    feat(account): add AccountController

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    /**
+     * @return string
+     */
+    public function index()
+    {
+        return 'AccountController@index';
+    }
+}
