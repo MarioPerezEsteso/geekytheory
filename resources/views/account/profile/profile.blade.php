5c6cc47b4f62ff71fcb3d9a48e19d7d5cbff6125
Mario Pérez Esteso <mario@geekytheory.com>
2017-09-18 18:26:04 +0200

    feat(accountDashboard): create profile page in account dashboard

--- resources/views/account/profile/profile.blade.php
+++ resources/views/account/profile/profile.blade.php
@@ -1,14 +1,14 @@
 @extends('account.layout')
 
-@section('title')
-    {{ trans('home.user_profile') }}
-@endsection
-
 @section('pageTitle')
     {{ trans('home.user_profile') }}
 @endsection
 
-@section('pageDescription')
+@section('contentTitle')
+    {{ trans('home.user_profile') }}
+@endsection
+
+@section('contentSubtitle')
     {{ trans('home.user_profile_page_description') }}
 @endsection
 
@@ -16,43 +16,72 @@
     <div class="row">
         <div class="col-md-4">
             {!! Form::open(['url' => 'account/profile', 'class' => 'form', 'method' => 'POST']) !!}
-
-            <div class="form-group">
-                {!! Form::label('name', trans('auth.name')) !!}
-                {!! Form::text('user[name]', $userProfile->name, ['class' => 'form-control', 'required' => 'required']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('email', trans('auth.email')) !!}
-                {!! Form::email('user[email]', $userProfile->email, ['class' => 'form-control', 'required' => 'required']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('username', trans('auth.username')) !!}
-                {!! Form::text('user[username]', $userProfile->username, ['class' => 'form-control', 'required' => 'required']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('biography', trans('auth.biography')) !!}
-                {!! Form::textarea('usermeta[biography]', $userProfile->userMeta->biography ?? '', ['class' => 'form-control', 'rows' => '4']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('job', trans('auth.job_position')) !!}
-                {!! Form::text('usermeta[job]', $userProfile->userMeta->job ?? '', ['class' => 'form-control']) !!}
-            </div>
-
-            <h3>{{ trans('public.social-networks') }}</h3>
-
-            @foreach(\App\UserMeta::$socialNetworks as $socialNetwork)
-                <div class="form-group">
-                    {!! Form::label($socialNetwork, trans('public.' . $socialNetwork)) !!}
-                    {!! Form::text("usermeta[$socialNetwork]", $userProfile->userMeta->$socialNetwork ?? '', ['class' => 'form-control', 'placeholder' => trans('public.' . $socialNetwork . '.placeholder')]) !!}
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="card-title">Sobre tí</h2>
+                    <small class="card-subtitle">
+                        Información sobre tu nombre de pila y nombre de usuario, correo electrónico y biografía.
+                    </small>
                 </div>
-            @endforeach
 
-            <div>
-                {!! Form::submit(trans('home.save'),['class' => 'btn btn-primary']) !!}
+                <div class="card-block">
+                    <div class="form-group form-group--float">
+                        {!! Form::text('user[name]', $userProfile->name, ['class' => 'form-control', 'required' => 'required']) !!}
+                        {!! Form::label('name', trans('auth.name')) !!}
+                        <i class="form-group__bar"></i>
+                    </div>
+
+                    <div class="form-group form-group--float">
+                        {!! Form::email('user[email]', $userProfile->email, ['class' => 'form-control', 'required' => 'required']) !!}
+                        {!! Form::label('email', trans('auth.email')) !!}
+                        <i class="form-group__bar"></i>
+                    </div>
+
+                    <div class="form-group form-group--float ">
+                        {!! Form::text('user[username]', $userProfile->username, ['class' => 'form-control', 'required' => 'required']) !!}
+                        {!! Form::label('username', trans('auth.username')) !!}
+                        <i class="form-group__bar"></i>
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('biography', trans('auth.biography')) !!}
+                        {!! Form::textarea('usermeta[biography]', $userProfile->userMeta->biography ?? '', ['class' => 'form-control', 'rows' => '4']) !!}
+                        <i class="form-group__bar"></i>
+                    </div>
+
+                    <div class="form-group form-group--float">
+                        {!! Form::text('usermeta[job]', $userProfile->userMeta->job ?? '', ['class' => 'form-control']) !!}
+                        {!! Form::label('job', trans('auth.job_position')) !!}
+                        <i class="form-group__bar"></i>
+                    </div>
+                </div>
+
+                <div class="card-block">
+                    {!! Form::button(trans('home.save'), ['class' => 'btn btn-primary waves-effect', 'type' => 'submit']) !!}
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="card-title">{{ trans('public.social-networks') }}</h2>
+                    <small class="card-subtitle">
+                        ¿En qué redes sociales publicas contenido?
+                    </small>
+                </div>
+
+                <div class="card-block">
+                    @foreach(\App\UserMeta::$socialNetworks as $socialNetwork)
+                        <div class="form-group form-group--float">
+                            {!! Form::text("usermeta[$socialNetwork]", $userProfile->userMeta->$socialNetwork ?? '', ['class' => 'form-control']) !!}
+                            {!! Form::label($socialNetwork, trans('public.' . $socialNetwork) . ' URL') !!}
+                            <i class="form-group__bar"></i>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div class="card-block">
+                    {!! Form::button(trans('home.save'), ['class' => 'btn btn-primary waves-effect', 'type' => 'submit']) !!}
+                </div>
             </div>
 
             {!! Form::close() !!}
