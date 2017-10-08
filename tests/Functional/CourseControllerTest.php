f23c05053cfcef7a5aee5f00695315a45aa43ca5
Mario Pérez Esteso <mario@geekytheory.com>
2017-10-08 20:18:22 +0200

    test(courses): test course join

--- tests/Functional/CourseControllerTest.php
+++ tests/Functional/CourseControllerTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Functional;
 
+use App\Course;
 use App\User;
+use Illuminate\Database\Eloquent\Collection;
+use Tests\Helpers\Response;
 use Tests\Helpers\TestUtils;
 use Tests\TestCase;
 
@@ -19,6 +22,13 @@ class CourseControllerTest extends TestCase
      * Single course endpoint.
      */
     protected $singleCourseEndpoint = 'curso/{slug}';
+
+    /**
+     * Join course endpoint.
+     *
+     * @var string
+     */
+    protected $joinCoursePostEndpoint = 'curso/{id}/matriculacion';
 
     /**
      * Test page single course ok.
@@ -74,12 +84,217 @@ class CourseControllerTest extends TestCase
     }
 
     /**
-     * Get an array of course statuses.
+     * Provider for method testGetSingleCourseNotPublishedErrorNotFound.
      *
      * @return array
      */
     public static function getCourseStatuses()
     {
         return [['draft'], ['pending'], ['scheduled'], ['deleted']];
+    }
+
+    /**
+     * Test join course ok.
+     *
+     * @dataProvider getJoinCourseOkTestData
+     * @param $courseFree
+     * @param $subscriptionFree
+     */
+    public function testJoinCourseOk($courseFree, $subscriptionFree)
+    {
+        // Prepare
+        /** @var Collection $courses */
+        $courses = TestUtils::createCoursesWithChaptersAndLessons(null, 1, 1, 1, ['free' => $courseFree]);
+        /** @var Course $course */
+        $course = $courses->first();
+        /** @var User $pupil */
+        $pupil = factory(User::class)->create([]);
+
+        if (!$subscriptionFree) {
+            // @TODO: subscribe user to paid plan
+        }
+
+        // Request
+        /** @var Response $response */
+        $response = $this->actingAs($pupil)->call('POST', TestUtils::createEndpoint($this->joinCoursePostEndpoint, ['id' => $course->id,]));
+
+        // Asserts
+        $response->assertStatus(200);
+
+        $response->assertExactJson(['joined' => 1]);
+
+        $this->assertDatabaseHas('users_courses', [
+            'user_id' => $pupil->id,
+            'course_id' => $course->id,
+        ]);
+    }
+
+    /**
+     * Test that the teacher of the course can join in.
+     *
+     * @dataProvider getJoinCourseOkTestData
+     * @param boolean $courseFree
+     * @param boolean $subscriptionFree
+     */
+    public function testTeacherCanJoinCourseOk($courseFree, $subscriptionFree)
+    {
+        // Prepare
+        /** @var User $teacher */
+        $teacher = factory(User::class)->create([]);
+
+        /** @var Collection $courses */
+        $courses = TestUtils::createCoursesWithChaptersAndLessons($teacher, 1, 1, 1, ['free' => $courseFree]);
+        /** @var Course $course */
+        $course = $courses->first();
+
+        if (!$subscriptionFree) {
+            // @TODO: subscribe user to paid plan
+        }
+
+        // Request
+        /** @var Response $response */
+        $response = $this->actingAs($teacher)->call('POST', TestUtils::createEndpoint($this->joinCoursePostEndpoint, ['id' => $course->id,]));
+
+        // Asserts
+        $response->assertStatus(200);
+
+        $response->assertExactJson(['joined' => 1]);
+
+        $this->assertDatabaseHas('users_courses', [
+            'user_id' => $teacher->id,
+            'course_id' => $course->id,
+        ]);
+    }
+
+    /**
+     * Data provider for testJoinCourseOk and testTeacherCanJoinCourseOk
+     *
+     * @return array
+     */
+    public static function getJoinCourseOkTestData()
+    {
+        return [
+            'Free course and free subscription' => [true, true,],
+            'Free course and premium subscription' => [true, false],
+            'Premium course and premium subscription' => [false, false,]
+        ];
+    }
+
+    /**
+     * Test that a user can't join a course that has not been published.
+     *
+     * @dataProvider getJoinCourseNonPublishedErrorTestData
+     * @param string $courseStatus
+     */
+    public function testJoinCourseNonPublishedError($courseStatus)
+    {
+        // Prepare
+        /** @var Collection $courses */
+        $courses = TestUtils::createCoursesWithChaptersAndLessons(null, 1, 1, 1, ['status' => $courseStatus]);
+
+        /** @var Course $course */
+        $course = $courses->first();
+
+        /** @var User $pupil */
+        $pupil = factory(User::class)->create([]);
+
+        // Request
+        /** @var Response $response */
+        $response = $this->actingAs($pupil)->call('POST', TestUtils::createEndpoint($this->joinCoursePostEndpoint, ['id' => $course->id,]));
+
+        // Asserts
+        $response->assertStatus(200);
+
+        $response->assertExactJson([
+            'joined' => 0,
+            'error' => trans('public.course_does_not_exist'),
+        ]);
+
+        $this->assertDatabaseMissing('users_courses', [
+            'user_id' => $pupil->id,
+            'course_id' => $course->id,
+        ]);
+    }
+
+    /**
+     * Data provider for testJoinCourseNonPublishedError.
+     *
+     * @return array
+     */
+    public static function getJoinCourseNonPublishedErrorTestData()
+    {
+        return [
+            'Course status draft' => ['draft'],
+            'Course status pending' => ['pending'],
+            'Course status scheduled' => ['scheduled'],
+            'Course status deleted' => ['deleted'],
+        ];
+    }
+
+    /**
+     * Test a user can't join a course that does not exist.
+     */
+    public function testJoinCourseThatDoesNotExistError()
+    {
+        // Prepare
+        /** @var User $pupil */
+        $pupil = factory(User::class)->create([]);
+
+        // Request
+        /** @var Response $response */
+        $response = $this->actingAs($pupil)->call('POST', TestUtils::createEndpoint($this->joinCoursePostEndpoint, ['id' => 999,]));
+
+        // Asserts
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Test that a user with free subscription can't join a premium course.
+     */
+    public function testJoinPremiumCourseWithFreeSubscriptionError()
+    {
+        // Prepare
+        /** @var Collection $courses */
+        $courses = TestUtils::createCoursesWithChaptersAndLessons(null, 1, 1, 1, ['free' => false]);
+        /** @var Course $course */
+        $course = $courses->first();
+        /** @var User $pupil */
+        $pupil = factory(User::class)->create([]);
+
+        // Request
+        /** @var Response $response */
+        $response = $this->actingAs($pupil)->call('POST', TestUtils::createEndpoint($this->joinCoursePostEndpoint, ['id' => $course->id,]));
+
+        // Asserts
+        $response->assertStatus(200);
+
+        $response->assertExactJson([
+            'joined' => 0,
+            'error' => trans('public.you_must_be_premium_subscriber'),
+        ]);
+
+        $this->assertDatabaseMissing('users_courses', [
+            'user_id' => $pupil->id,
+            'course_id' => $course->id,
+        ]);
+    }
+
+    /**
+     * Test join course redirects to login if user is not logged in.
+     */
+    public function testJoinCourseNonLoggedUserNotAuthorized()
+    {
+        // Prepare
+        /** @var Collection $courses */
+        $courses = TestUtils::createCoursesWithChaptersAndLessons();
+        /** @var Course $course */
+        $course = $courses->first();
+
+        // Request
+        /** @var Response $response */
+        $response = $this->call('POST', TestUtils::createEndpoint($this->joinCoursePostEndpoint, ['id' => $course->id,]));
+
+        // Asserts
+        $response->assertRedirect('login');
     }
 }
