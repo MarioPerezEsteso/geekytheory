7b37c2d67277223f29bdaac99e43b03553a74e08
Mario Pérez Esteso <mario@geekytheory.com>
2017-10-08 20:17:00 +0200

    feat(courses): create method for a user to join a course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Course;
-use App\Exceptions\ExceptionFactory;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CourseController extends Controller
 {
@@ -18,5 +20,41 @@ class CourseController extends Controller
         $course = Course::getPublished(['slug' => $slug])->with('teacher')->with('chapters.lessons')->firstOrFail();
 
         return view('courses.single', compact('course'));
+    }
+
+
+    /**
+     * Join a course.
+     *
+     * @param Request $request
+     * @param $id
+     * @return array
+     */
+    public function join(Request $request, $id)
+    {
+        /** @var Course $course */
+        $course = Course::findOrFail($id);
+
+        /** @var User $user */
+        $user = Auth::user();
+
+        // Check if the course is published
+        if (!$course->isPublished()) {
+            return [
+                'joined' => 0,
+                'error' => trans('public.course_does_not_exist'),
+            ];
+        }
+
+        if (!policy($course)->join($user, $course)) {
+            return [
+                'joined' => 0,
+                'error' => trans('public.you_must_be_premium_subscriber'),
+            ];
+        }
+
+        $user->courses()->attach($course->id);
+
+        return ['joined' => 1];
     }
 }
