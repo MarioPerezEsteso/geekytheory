aeff2e95ab4d01c7ad264e4e8f90abf8c7bbae6c
Mario Pérez Esteso <mario@geekytheory.com>
2017-10-15 17:45:14 +0200

    feat(subscription): create routes for subscription

--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,12 @@ Route::get('/', 'IndexController@index');
  * Authentication routes
  */
 Route::get('login', [
-    'as' =>'login',
+    'as' => 'login',
     'uses' => 'Auth\LoginController@showLoginForm',
 ]);
 
 Route::post('login', [
-    'as' =>'login',
+    'as' => 'login',
     'uses' => 'Auth\LoginController@login'
 ]);
 
@@ -41,15 +41,15 @@ Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']
  * Registration routes
  */
 Route::get('registro', [
-    'as'            => 'auth.register.get',
-    'middleware'    => 'allow_user_registration',
-    'uses'          => 'Auth\RegisterController@showRegistrationForm'
+    'as' => 'auth.register.get',
+    'middleware' => 'allow_user_registration',
+    'uses' => 'Auth\RegisterController@showRegistrationForm'
 ]);
 
 Route::post('register', [
-    'as'            => 'auth.register.post',
-    'middleware'    => 'allow_user_registration',
-    'uses'          => 'Auth\RegisterController@register'
+    'as' => 'auth.register.post',
+    'middleware' => 'allow_user_registration',
+    'uses' => 'Auth\RegisterController@register'
 ]);
 
 /**
@@ -106,6 +106,18 @@ Route::post('account/profile/password', [
     'uses' => 'UserController@updatePassword',
 ]);
 
+Route::get('cuenta/suscripcion', [
+    'as' => 'account.subscription',
+    'middleware' => 'auth',
+    'uses' => 'SubscriptionController@show',
+]);
+
+Route::post('account/subscription', [
+    'as' => 'account.subscription.post',
+    'middleware' => 'auth',
+    'uses' => 'SubscriptionController@store',
+]);
+
 /*
  * Newsletter subscription routes
  */
@@ -121,244 +133,244 @@ Route::group(['prefix' => 'newsletter'], function () {
 Route::get('home', 'Home\HomeController@index');
 
 Route::get('home/articles/create', [
-    'middleware'    => 'auth',
-    'uses'          => 'ArticleController@create'
+    'middleware' => 'auth',
+    'uses' => 'ArticleController@create'
 ]);
 
 Route::get('home/articles/edit/{id?}', [
-    'middleware'    => 'auth',
-    'uses'          => 'ArticleController@edit'
+    'middleware' => 'auth',
+    'uses' => 'ArticleController@edit'
 ]);
 
 Route::post('home/articles/update/{id?}', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'ArticleController@update'
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'ArticleController@update'
 ]);
 
 Route::get('home/articles/{username?}', [
-    'middleware'    => 'auth',
-    'uses'          => 'ArticleController@indexHome'
+    'middleware' => 'auth',
+    'uses' => 'ArticleController@indexHome'
 ]);
 
 Route::post('home/articles/store', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'ArticleController@store'
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'ArticleController@store'
 ]);
 
 Route::get('home/articles/preview/{slug}', [
-    'middleware'    => 'auth',
-    'uses'          => 'ArticleController@preview'
+    'middleware' => 'auth',
+    'uses' => 'ArticleController@preview'
 ]);
 
 Route::get('home/pages/create', [
-    'middleware'    => 'auth',
-    'uses'          => 'PageController@create'
+    'middleware' => 'auth',
+    'uses' => 'PageController@create'
 ]);
 
 Route::get('home/pages/edit/{id?}', [
-    'middleware'    => 'auth',
-    'uses'          => 'PageController@edit'
+    'middleware' => 'auth',
+    'uses' => 'PageController@edit'
 ]);
 
 Route::post('home/pages/update/{id}', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'PageController@update'
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'PageController@update'
 ]);
 
 Route::get('home/pages/{username?}', [
-    'middleware'    => 'auth',
-    'uses'          => 'PageController@indexHome'
+    'middleware' => 'auth',
+    'uses' => 'PageController@indexHome'
 ]);
 
 Route::post('home/pages/store', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'PageController@store'
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'PageController@store'
 ]);
 
 Route::get('home/pages/preview/{slug}', [
-    'middleware'    => 'auth',
-    'uses'          => 'PageController@preview'
+    'middleware' => 'auth',
+    'uses' => 'PageController@preview'
 ]);
 
 Route::get('home/posts/delete/{id}', [
-    'middleware'    => 'auth',
-    'uses'          => 'PostController@delete'
+    'middleware' => 'auth',
+    'uses' => 'PostController@delete'
 ]);
 
 Route::get('home/posts/restore/{id}', [
-    'middleware'    => 'auth',
-    'uses'          => 'PostController@restore'
+    'middleware' => 'auth',
+    'uses' => 'PostController@restore'
 ]);
 
 Route::get('home/tags', [
-    'middleware'    => 'auth',
-    'uses'          => 'TagController@create'
+    'middleware' => 'auth',
+    'uses' => 'TagController@create'
 ]);
 
 Route::get('home/tags/edit/{id}', [
-    'middleware'    => 'auth',
-    'uses'          => 'TagController@edit'
+    'middleware' => 'auth',
+    'uses' => 'TagController@edit'
 ]);
 
 Route::get('home/tags/delete/{id}', [
-    'middleware'    => 'auth',
-    'uses'          => 'TagController@destroy'
+    'middleware' => 'auth',
+    'uses' => 'TagController@destroy'
 ]);
 
 Route::post('home/tags/store', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'TagController@store'
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'TagController@store'
 ]);
 
 Route::post('home/tags/update/{id}', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'TagController@update'
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'TagController@update'
 ]);
 
 Route::get('home/categories', [
-    'middleware'    => 'auth',
-    'uses'          => 'CategoryController@create'
+    'middleware' => 'auth',
+    'uses' => 'CategoryController@create'
 ]);
 
 Route::get('home/categories/edit/{id}', [
-    'middleware'    => 'auth',
-    'uses'          => 'CategoryController@edit'
+    'middleware' => 'auth',
+    'uses' => 'CategoryController@edit'
 ]);
 
 Route::get('home/categories/delete/{id}', [
-    'middleware'    => 'auth',
-    'uses'          => 'CategoryController@destroy'
+    'middleware' => 'auth',
+    'uses' => 'CategoryController@destroy'
 ]);
 
 Route::post('home/categories/store', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'CategoryController@store'
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'CategoryController@store'
 ]);
 
 Route::post('home/categories/update/{id}', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'CategoryController@update'
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'CategoryController@update'
 ]);
 
 Route::post('home/categories/delete-image', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'CategoryController@deleteImage',
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'CategoryController@deleteImage',
 ]);
 
 Route::get('home/galleries/{username?}', [
-    'middleware'    => 'auth',
-    'uses'          => 'GalleryController@indexHome',
+    'middleware' => 'auth',
+    'uses' => 'GalleryController@indexHome',
 ]);
 
 Route::post('home/gallery/store', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'GalleryController@store',
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'GalleryController@store',
 ]);
 
 Route::get('home/gallery/create', [
-    'middleware'    => 'auth',
-    'uses'          => 'GalleryController@create',
+    'middleware' => 'auth',
+    'uses' => 'GalleryController@create',
 ]);
 
 Route::get('home/gallery/edit/{id}', [
-    'middleware'    => 'auth',
-    'uses'          => 'GalleryController@edit',
+    'middleware' => 'auth',
+    'uses' => 'GalleryController@edit',
 ]);
 
 Route::post('home/gallery/update/{id}', [
-    'middleware'    => 'auth',
-    'uses'          => 'GalleryController@update',
+    'middleware' => 'auth',
+    'uses' => 'GalleryController@update',
 ]);
 
 Route::post('home/gallery/image/delete', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'ImageController@deleteImageFromGallery',
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'ImageController@deleteImageFromGallery',
 ]);
 
 Route::get('home/articles/imagemanager/upload', [
-    'middleware'    => 'auth',
-    'uses'          => 'ImageManagerController@create'
+    'middleware' => 'auth',
+    'uses' => 'ImageManagerController@create'
 ]);
 
 Route::get('home/articles/edit/imagemanager/upload', [
-    'middleware'    => 'auth',
-    'uses'          => 'ImageManagerController@create'
+    'middleware' => 'auth',
+    'uses' => 'ImageManagerController@create'
 ]);
 
 Route::get('home/pages/imagemanager/upload', [
-    'middleware'    => 'auth',
-    'uses'          => 'ImageManagerController@create'
+    'middleware' => 'auth',
+    'uses' => 'ImageManagerController@create'
 ]);
 
 Route::get('home/pages/edit/imagemanager/upload', [
-    'middleware'    => 'auth',
-    'uses'          => 'ImageManagerController@create'
+    'middleware' => 'auth',
+    'uses' => 'ImageManagerController@create'
 ]);
 
 Route::post('home/posts/delete-image', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'PostController@deletePostImage',
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'PostController@deletePostImage',
 ]);
 
 Route::post('home/imagemanager/upload', [
-    'middleware'    => 'auth',
-    'uses'          => 'ImageManagerController@store',
+    'middleware' => 'auth',
+    'uses' => 'ImageManagerController@store',
 ]);
 
 Route::get('home/sitemeta', [
-    'middleware'    => 'auth',
-    'uses'          => 'SiteMetaController@edit',
+    'middleware' => 'auth',
+    'uses' => 'SiteMetaController@edit',
 ]);
 
 Route::post('home/sitemeta/update', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'SiteMetaController@update',
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'SiteMetaController@update',
 ]);
 
 Route::post('home/sitemeta/delete-image', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'SiteMetaController@deleteImage',
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'SiteMetaController@deleteImage',
 ]);
 
 Route::get('home/menu', [
-    'middleware'    => 'auth',
-    'uses'          => 'SiteMetaController@editMenu',
+    'middleware' => 'auth',
+    'uses' => 'SiteMetaController@editMenu',
 ]);
 
 Route::post('home/menu/update', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'SiteMetaController@updateMenu',
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'SiteMetaController@updateMenu',
 ]);
 
 Route::get('home/menu/getNewMenuItemHtml', [
-    'middleware'    => 'auth',
-    'before'        => 'csrf',
-    'uses'          => 'SiteMetaController@getNewMenuItemHtml',
+    'middleware' => 'auth',
+    'before' => 'csrf',
+    'uses' => 'SiteMetaController@getNewMenuItemHtml',
 ]);
 
 Route::post('comment/store', [
-    'before'        => 'csrf',
-    'uses'          => 'CommentController@store',
+    'before' => 'csrf',
+    'uses' => 'CommentController@store',
 ]);
 
 Route::get('comment/getForm', [
-    'before'        => 'csrf',
-    'uses'          => 'CommentController@getForm',
+    'before' => 'csrf',
+    'uses' => 'CommentController@getForm',
 ]);
 
 Route::get('feed', 'FeedController@feed');
@@ -366,8 +378,8 @@ Route::get('feed', 'FeedController@feed');
 Route::get('/{slug?}', 'ArticleController@show');
 
 Route::post('share-article', [
-    'before'        => 'csrf',
-    'uses'			=> 'ArticleController@updateShares'
+    'before' => 'csrf',
+    'uses' => 'ArticleController@updateShares'
 ]);
 
 Route::get('/p/{slug?}', 'PageController@show');
