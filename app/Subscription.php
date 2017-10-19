d2c4b97ddf87ce112bb97319d6e56c9e12996965
Mario Pérez Esteso <mario@geekytheory.com>
2017-10-19 17:43:01 +0000

    feat(payments): rename subscriptions

--- app/Subscription.php
+++ app/Subscription.php
@@ -6,9 +6,9 @@ class Subscription extends \Laravel\Cashier\Subscription
 {
     const PLAN_MONTHLY = 'monthly';
     const PLAN_MONTHLY_PRICE_EUR = 15;
-    const PLAN_MONTHLY_NAME = 'Suscripción mensual';
+    const PLAN_MONTHLY_NAME = 'Monthly subscription';
 
     const PLAN_YEARLY = 'yearly';
     const PLAN_YEARLY_PRICE_EUR = 150;
-    const PLAN_YEARLY_NAME = 'Suscripción anual';
+    const PLAN_YEARLY_NAME = 'Yearly subscription';
 }
