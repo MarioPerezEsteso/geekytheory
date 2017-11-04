71b4788533118abe50367f9ac14533a8e048b1f8
Mario Pérez Esteso <mario@geekytheory.com>
2017-11-04 12:37:38 +0100

    feat(register): make the register form a partial to reuse it

--- resources/views/courses/partials/lesson/headerRegister.blade.php
+++ resources/views/courses/partials/lesson/headerRegister.blade.php
@@ -6,34 +6,7 @@
                 <p>Este curso es gratuito y podrás verlo siempre que quieras una vez hayas creado tu cuenta.</p>
             </div>
             <div class="col-lg-5 col-lg-push-1">
-                <div class="login-jumbotron">
-                    <div class="login-jumbotron-trial-banner">
-                        <p>Pruébalo gratis durante 7 días</p>
-                    </div>
-                    <form class="login-form">
-                        <div class="form-group">
-                            <input type="text" class="form-control login-field" value=""
-                                   placeholder="Introduce tu nombre"
-                                   id="login-name">
-                            <label class="login-field-icon fui-user" for="login-name"></label>
-                        </div>
-
-                        <div class="form-group">
-                            <input type="text" class="form-control login-field" value=""
-                                   placeholder="Introduce tu email"
-                                   id="login-name">
-                            <label class="login-field-icon fui-mail" for="login-name"></label>
-                        </div>
-
-                        <div class="form-group">
-                            <input type="password" class="form-control login-field" value="" placeholder="Contraseña"
-                                   id="login-pass">
-                            <label class="login-field-icon fui-lock" for="login-pass"></label>
-                        </div>
-
-                        <a class="btn btn-primary btn-lg btn-block" href="#">Obtener 7 días gratis</a>
-                    </form>
-                </div>
+                @include('courses.partials.auth.register')
             </div>
         </div>
     </div>
