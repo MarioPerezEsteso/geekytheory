891b25fffd869c296c8db188beda63122cbcc913
Mario Pérez Esteso <mario@geekytheory.com>
2017-11-18 13:58:47 +0100

    feat(courses): move join course header to partial

--- resources/views/courses/single.blade.php
+++ resources/views/courses/single.blade.php
@@ -30,23 +30,7 @@
 
 @include('courses.partials.navbar')
 
-<div class="jumbotron jumbotron-course center-flex gradient-blue-geeky">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <span class="btn btn-info">{{ trans('public.' . $course->difficulty) }}</span>
-                <h1 class="jumbotron-title">{{ $course->title }}</h1>
-                <p>{!! $course->description !!}</p>
-                {!! Form::open(['url' => route('course.join.post', ['id' => $course->id])]) !!}
-                {!! Form::submit("Apuntarme" ,['class' => 'btn btn-primary btn-join-course']) !!}
-                {!! Form::close() !!}
-            </div>
-            <div class="col-lg-5 col-lg-push-1">
-                <img class="img-responsive" src="{{ $course->image_thumbnail }}">
-            </div>
-        </div>
-    </div>
-</div>
+@include('courses.partials.course.headerJoinCourse')
 
 <section class="container">
     <div class="row">
