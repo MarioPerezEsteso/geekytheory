36039f1e02c6538551cc01efd947995ace3d5722
Mario Pérez Esteso <mario@geekytheory.com>
2017-11-28 18:26:37 +0100

    feat(faq): add FAQs

--- resources/views/courses/pricing.blade.php
+++ resources/views/courses/pricing.blade.php
@@ -87,6 +87,107 @@
     </div>
 </section>
 
+<section class="container-fluid section-faq">
+    <div class="container">
+        <hr/>
+        <div class="row">
+            <div class="col-lg-12">
+                <h5>Preguntas y respuestas</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-faq">
+                            <div class="panel-body">
+                                <div class="panel-question">
+                                    ¿Cómo funciona la suscripción?
+                                </div>
+                                <div class="panel-answer">
+                                    Se te harán cobros automáticos a tu tarjeta mensualmente a menos que canceles tu suscripción.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-faq">
+                            <div class="panel-body">
+                                <div class="panel-question">
+                                    ¿Tengo permanencia en mi suscripción?
+                                </div>
+                                <div class="panel-answer">
+                                    No hay ningún compromiso de permanencia con Geeky Theory. Tú decides cuándo darte de alta y cuándo darte de baja.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-faq">
+                            <div class="panel-body">
+                                <div class="panel-question">
+                                    ¿Qué conocimientos necesito para empezar a aprender en Geeky Theory?
+                                </div>
+                                <div class="panel-answer">
+                                    ¡Ninguno! En Geeky Theory enseñamos a principiantes y a expertos. Añadimos cursos continuamente para que aprendas lo máximo posible.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-faq">
+                            <div class="panel-body">
+                                <div class="panel-question">
+                                    ¿Cuánto tiempo tardaré hasta desarrollar aplicaciones por mi cuenta?
+                                </div>
+                                <div class="panel-answer">
+                                    Cada persona aprende a un ritmo diferente, pero si dedicas varias horas al día, podrás programar tus propias aplicaciones en cuestión de meses.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-faq">
+                            <div class="panel-body">
+                                <div class="panel-question">
+                                    ¿Cada cuánto tiempo publicáis nuevos cursos?
+                                </div>
+                                <div class="panel-answer">
+                                    Mensualmente. Sin embargo, intentaremos que sea de forma semanal con tu ayuda.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-faq">
+                            <div class="panel-body">
+                                <div class="panel-question">
+                                    ¿Necesito un sistema operativo o hardware específico?
+                                </div>
+                                <div class="panel-answer">
+                                    No. Intentamos que puedas aprender con nosotros en cualquier ordenador, sin importar el que sea.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @include('courses.partials.footer')
 
 <script src="assets/vendor/flat-ui/dist/js/vendor/jquery.min.js"></script>
