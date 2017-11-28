d28a8a3d8c065afeefc7a0342a222ab81f055fc8
Mario Pérez Esteso <mario@geekytheory.com>
2017-11-28 19:10:51 +0100

    feat(testimonials): add testimonials

--- resources/views/courses/pricing.blade.php
+++ resources/views/courses/pricing.blade.php
@@ -41,13 +41,12 @@
             <div class="col-md-4">
                 <div class="testimonials">
                     <div class="active item">
-                        <blockquote><p>Denim you probably haven't heard of. Lorem ipsum dolor met consectetur
-                                adipisicing sit amet, consectetur adipisicing elit...</p></blockquote>
+                        <blockquote><p>Con Geeky Theory he aprendido a realizar mi propia página web y ahora estoy al tanto de todas las nuevas tecnologías</p></blockquote>
                         <div class="carousel-info">
-                            <img alt="" src="http://keenthemes.com/assets/bootsnipp/img1-small.jpg" class="pull-left">
+                            <img alt="" src="https://s.gravatar.com/avatar/2f78bd9412c33c6cd2b9338a0c3cc66f?s=200" class="pull-left">
                             <div class="pull-left">
-                                <span class="testimonials-name">Lina Mars</span>
-                                <span class="testimonials-post">Commercial Director</span>
+                                <span class="testimonials-name">Mercedes Muela</span>
+                                <span class="testimonials-post">Arquitecta</span>
                             </div>
                         </div>
                     </div>
@@ -56,13 +55,12 @@
             <div class="col-md-4">
                 <div class="testimonials">
                     <div class="active item">
-                        <blockquote><p>consectetur adipisicing elit, of them jean shorts sed magna
-                                aliqua. Lorem ipsum dolor met. lorem ipsum</p></blockquote>
+                        <blockquote><p>Geeky Theory es posiblemente la mejor plataforma de aprendizaje online para convertirse en un programador experto</p></blockquote>
                         <div class="carousel-info">
-                            <img alt="" src="http://keenthemes.com/assets/bootsnipp/img1-small.jpg" class="pull-left">
+                            <img alt="" src="https://www.gravatar.com/avatar/c5e4f29f7c876fd40102d73a87025a96?s=200" class="pull-left">
                             <div class="pull-left">
-                                <span class="testimonials-name">Lina Mars</span>
-                                <span class="testimonials-post">Commercial Director</span>
+                                <span class="testimonials-name">Miguel Catalan</span>
+                                <span class="testimonials-post">Android Engineer at Product Team, Cabify</span>
                             </div>
                         </div>
                     </div>
@@ -71,13 +69,12 @@
             <div class="col-md-4">
                 <div class="testimonials">
                     <div class="active item">
-                        <blockquote><p>Denim you probably haven't heard of. Lorem ipsum dolor met consectetur
-                                adipisicing sit amet, consectetur adipisicing elit</p></blockquote>
+                        <blockquote><p>Recomiendo Geeky Theory a todo aquel que quiera mejorar su carrera profesional viendo contenido de calidad</p></blockquote>
                         <div class="carousel-info">
-                            <img alt="" src="http://keenthemes.com/assets/bootsnipp/img1-small.jpg" class="pull-left">
+                            <img alt="" src="https://www.gravatar.com/avatar/923ae129bd4ad8566ed65a88076abcd6?s=200" class="pull-left">
                             <div class="pull-left">
-                                <span class="testimonials-name">Lina Mars</span>
-                                <span class="testimonials-post">Commercial Director</span>
+                                <span class="testimonials-name">Javier Andrés</span>
+                                <span class="testimonials-post">ITS Software Developer, GMV</span>
                             </div>
                         </div>
                     </div>
