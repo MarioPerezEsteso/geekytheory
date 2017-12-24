14cc7ac2567e42ec355df0e30c67f90269b11682
Mario Pérez Esteso <mario@geekytheory.com>
2017-12-24 17:22:36 +0100

    feat(lesson): create lesson policy to complete lesson

---
+++ app/Policies/LessonPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Lesson;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class LessonPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Policy to check if a user can mark a lesson as completed.
+     *
+     * @param User $user
+     * @param Lesson $lesson
+     * @return bool
+     */
+    public function complete(User $user, Lesson $lesson)
+    {
+        $userHasJoinedCourse = $user->hasJoinedCourse($lesson->chapter->course);
+        $courseIsPremium = $lesson->chapter->course->isPremium();
+        $lessonIsPremium = $lesson->isPremium();
+        $userHasSubscriptionActive = $user->hasSubscriptionActive();
+
+        return !$userHasJoinedCourse && $courseIsPremium && !$lessonIsPremium
+            || $userHasJoinedCourse && !$courseIsPremium && !$lessonIsPremium
+            || $userHasJoinedCourse && $courseIsPremium && $userHasSubscriptionActive;
+    }
+}
