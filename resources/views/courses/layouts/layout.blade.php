a83031d0c46126f3d7ecaf88227d37aa0a17b68f
Mario Pérez Esteso <mario@geekytheory.com>
2018-01-06 16:53:18 +0000

    feat(seo): add layout for courses

---
+++ resources/views/courses/layouts/layout.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta name="description" content="@yield('description')"/>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Loading Bootstrap -->
+    <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
+
+    <!-- Loading Flat UI -->
+    <link href="/assets/vendor/flat-ui/dist/css/flat-ui.css" rel="stylesheet">
+    <link href="{{ autoVersion('/assets/courses/css/app.css') }}" rel="stylesheet">
+
+    <link rel="shortcut icon" href="/assets/vendor/flat-ui/img/favicon.ico">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+    <script src="/assets/vendor/flat-ui/dist/js/vendor/html5shiv.js"></script>
+    <script src="/assets/vendor/flat-ui/dist/js/vendor/respond.min.js"></script>
+    <![endif]-->
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400' rel='stylesheet' type='text/css'>
+
+    @if (!empty($siteMeta->analytics_script))
+        {!! $siteMeta->analytics_script !!}
+    @endif
+
+    @include('courses.partials.cookies')
+    @include('courses.partials.hotjar')
+</head>
+<body>
+
+@include('courses.partials.navbar')
+
+@yield('content')
+
+@include('courses.partials.footer')
+
+<script src="/assets/vendor/flat-ui/dist/js/vendor/jquery.min.js"></script>
+<script src="/assets/vendor/flat-ui/dist/js/vendor/video.js"></script>
+<script src="/assets/vendor/flat-ui/dist/js/flat-ui.min.js"></script>
+<script src="/assets/vendor/flat-ui/docs/assets/js/application.js"></script>
+<script src="/assets/courses/js/drift.js" async></script>
+
+</body>
+</html>
