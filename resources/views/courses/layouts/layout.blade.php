5ddbef8adedd0ba2fed4ce1d2ddb0c80d85cc35c
Mario Pérez Esteso <mario@geekytheory.com>
2018-01-06 17:30:45 +0000

    feat(seo): add canonical links

--- resources/views/courses/layouts/layout.blade.php
+++ resources/views/courses/layouts/layout.blade.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="canonical" href="{{ URL::current() }}"/>
+
     <!-- Favicons -->
     <link rel="shortcut icon" href="{{ \App\Http\Controllers\ImageManagerController::getPublicImageUrl($siteMeta->favicon) }}">
     <link rel="apple-touch-icon" href="{{ \App\Http\Controllers\ImageManagerController::getPublicImageUrl($siteMeta->logo_57) }}">
