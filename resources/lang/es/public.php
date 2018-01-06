75daf50ef39efb8e199991a01ac4347f6b598d87
Mario Pérez Esteso <mario@geekytheory.com>
2018-01-06 16:53:52 +0000

    feat(seo): add pricing description

--- resources/lang/es/public.php
+++ resources/lang/es/public.php
@@ -81,4 +81,5 @@ return [
     'obtain' => 'Obtener',
     'i_want_it' => '¡Lo quiero!',
     'you_have_it' => 'Lo tienes',
+    'pricing_description' => 'Aprende a programar desde cero con Geeky Theory y los curso de Vagrant, Laravel, Javascript, VueJS, PHP, SQL y muchas tecnologías.',
 ];
