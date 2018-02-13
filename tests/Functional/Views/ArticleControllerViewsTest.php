3cbbccdd599e802c602cd6853cfc4c2549e77d8f
Mario Pérez Esteso <mario@geekytheory.com>
2018-02-13 18:05:40 +0000

    test(articleView): test that only admin user can access to some pages

--- tests/Functional/Views/ArticleControllerViewsTest.php
+++ tests/Functional/Views/ArticleControllerViewsTest.php
@@ -2,46 +2,56 @@
 
 namespace Tests\Functional\Views;
 
+use App\Post;
 use App\User;
+use Tests\Helpers\TestUtils;
 use Tests\TestCase;
 
 class ArticleControllerViewsTest extends TestCase
 {
     /**
-     * URL of the page to create articles.
-     *
-     * @var string
-     */
-    private $articleCreatePageUrl = 'home/articles/create';
-
-    /**
      * Test administrator user can visit the article create page.
+     *
+     * @dataProvider providerArticleGETPages
+     * @param string $page
      */
-    public function testUserAdministratorCanVisitArticleCreatePage()
+    public function testUserAdministratorCanVisitPages(string $page)
     {
         // Prepare
         /** @var User $user */
         $user = factory(User::class)->create();
+        $article = factory(Post::class)->create([
+            'status' => 'draft',
+        ]);
+
+        $page = TestUtils::createEndpoint($page, ['id' => $article->id, 'slug' => $article->slug,]);
 
         // Request
-        $response = $this->actingAs($user)->call('GET', $this->articleCreatePageUrl);
+        $response = $this->actingAs($user)->call('GET', $page);
 
         // Asserts
         $response->assertStatus(200);
-        $response->assertResponseIsView('home.posts.article');
     }
 
     /**
      * Test non administrator user can't visit the article create page.
+     *
+     * @dataProvider providerArticleGETPages
+     * @param string $page
      */
-    public function testUserNonAdministratorCannotVisitArticleCreatePage()
+    public function testUserNonAdministratorCannotVisitPages(string $page)
     {
         // Prepare
         /** @var User $user */
         $user = factory(User::class)->create(['is_admin' => false,]);
+        $article = factory(Post::class)->create([
+            'status' => 'draft',
+        ]);
+
+        $page = TestUtils::createEndpoint($page, ['id' => $article->id, 'slug' => $article->slug,]);
 
         // Request
-        $response = $this->actingAs($user)->call('GET', $this->articleCreatePageUrl);
+        $response = $this->actingAs($user)->call('GET', $page);
 
         // Asserts
         $response->assertStatus(404);
@@ -49,13 +59,48 @@ class ArticleControllerViewsTest extends TestCase
 
     /**
      * Test non administrator user can't visit the article create page.
+     *
+     * @dataProvider providerArticleGETPages
+     * @param string $page
      */
-    public function testNonLoggedUserCannotVisitArticleCreatePage()
+    public function testNonLoggedUserCannotVisitArticleCreatePage(string $page)
     {
+        // Prepare
+        $article = factory(Post::class)->create([
+            'status' => 'draft',
+        ]);
+
+        $page = TestUtils::createEndpoint($page, ['id' => $article->id, 'slug' => $article->slug,]);
+
         // Request
-        $response = $this->call('GET', $this->articleCreatePageUrl);
+        $response = $this->call('GET', $page);
 
         // Asserts
-        $response->assertStatus(404);
+        $response->assertRedirect($this->loginUrl);
+    }
+
+    public function providerArticleGETPages(): array
+    {
+        return [
+            [
+                'home/articles'
+            ], [
+                'home/articles/create',
+            ], [
+                'home/articles/edit/{id}'
+            ], [
+                'home/articles/preview/{slug}'
+            ], [
+                'home/posts/delete/{id}',
+            ], [
+                'home/posts/restore/{id}',
+            ], [
+                'home/articles/imagemanager/upload',
+            ], [
+                'home/articles/edit/imagemanager/upload'
+            ], [
+
+            ],
+        ];
     }
 }
