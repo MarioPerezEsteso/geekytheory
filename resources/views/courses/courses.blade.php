65b766b1e785a4b9059056d8b4435aeb316d6ecb
Mario Pérez Esteso <mario@geekytheory.com>
2018-02-13 18:07:08 +0000

    feat(views): check if variable is set before using it

--- resources/views/courses/courses.blade.php
+++ resources/views/courses/courses.blade.php
@@ -12,11 +12,14 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="{{ $siteMeta->title }}">
     <meta name="twitter:description" content="{{ $siteMeta->description }}">
-    <meta name="twitter:image" content="{{ \App\Http\Controllers\ImageManagerController::getPublicImageUrl('images/geekytheory-twitter.jpg', false, true) }}">
+    <meta name="twitter:image"
+          content="{{ \App\Http\Controllers\ImageManagerController::getPublicImageUrl('images/geekytheory-twitter.jpg', false, true) }}">
 @endsection
 
 @section('content')
-    <section class="container-fluid section-gray section-courses">
-        @include('courses.partials.courses')
-    </section>
+    @if (!empty($courses))
+        <section class="container-fluid section-gray section-courses">
+            @include('courses.partials.courses')
+        </section>
+    @endif
 @endsection
