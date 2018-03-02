e080ab2d8451fc3a5f80cb2ca128ba8b94e439ad
Mario Pérez Esteso <mario@geekytheory.com>
2018-03-02 20:23:41 +0100

    feat(faq): change faq title

--- resources/views/courses/partials/pricing/faq.blade.php
+++ resources/views/courses/partials/pricing/faq.blade.php
@@ -1,8 +1,7 @@
 <div class="container">
-    <hr/>
     <div class="row">
         <div class="col-lg-12">
-            <h5>Preguntas y respuestas</h5>
+            <h5>Preguntas frecuentes</h5>
         </div>
     </div>
     <div class="row">
