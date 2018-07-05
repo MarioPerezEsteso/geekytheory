bec6d78a0400720b183c4d65120244f11db38e8f
Mario Pérez Esteso <mario@geekytheory.com>
2018-07-05 16:39:01 +0000

    feat(courses): create partial for go premium layout

---
+++ resources/views/web/partials/gopremiumsquare.blade.php
@@ -0,0 +1,22 @@
+<section class="slice-xl sct-color-3" style="padding-top: 80px;">
+    <div class="shape-container" data-shape-fill="sct-color-1" data-shape-style="opaque_waves_1"
+         data-shape-position="top" style="height: 80px;">
+        <svg class="shape-item" fill="#ffffff" xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 1000 100" preserveAspectRatio="none">
+            <path d="M 0 0 c 0 0 200 50 500 50 s 500 -50 500 -50 v 101 h -1000 v -100 z"></path>
+        </svg>
+    </div>
+
+    <div class="container">
+        <div class="cta-container cta-container-overlay">
+            <div class="cta-block cta-block-overlay bg-base-1 z-depth-5 text-center">
+                <h2 class="heading heading-inverse heading-2 strong-500">
+                    ¿Quieres aprender sin límites? Lo tuyo es Premium.
+                </h2>
+                <a href="#" class="btn btn-styled btn-lg btn-white btn-outline btn-circle mt-5">
+                    Pásate a Premium
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
