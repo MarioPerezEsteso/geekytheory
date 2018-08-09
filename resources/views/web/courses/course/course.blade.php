ad91205463047a72e9ebe5823dbb8242278bc663
Mario Pérez Esteso <mario@geekytheory.com>
2018-08-09 18:26:41 +0000

    feat(courses): make course page responsive

--- resources/views/web/courses/course/course.blade.php
+++ resources/views/web/courses/course/course.blade.php
@@ -58,11 +58,11 @@
                         @foreach($chapter->lessons as $lesson)
                             <div class="block-footer border-top">
                                 <div class="row h-100">
-                                    <div class="col-lg-9 col-sm-12">
+                                    <div class="col-lg-9 col-md-10 col-sm-9 col-12">
                                         <div class="row">
-                                            <div class="col-lg-1 col-sm-2 my-auto">
+                                            <div class="col-lg-1 col-md-2 col-3 my-auto">
                                                 @if (!is_null($lesson->icon))
-                                                    <img src="{{ \App\Http\Controllers\ImageManagerController::getPublicImageUrl($lesson->icon, false, true) }}"/>
+                                                    <img class="lesson-icon" src="{{ \App\Http\Controllers\ImageManagerController::getPublicImageUrl($lesson->icon, false, true) }}"/>
                                                     @if ($lesson->completed === true)
                                                         <i class="lesson-completed-icon lesson-completed-icon-over-icon fa fa-check-circle"></i>
                                                     @endif
@@ -72,7 +72,7 @@
                                                     @endif
                                                 @endif
                                             </div>
-                                            <div class="col-lg-11 col-sm-10 my-auto">
+                                            <div class="col-9 col-lg-11 my-auto">
                                                 <a href="{{ route('course.lesson', ['courseSlug' => $course->slug, 'lessonSlug' => $lesson->slug]) }}"
                                                    class="lesson-link">
                                                     {{ $chapter->order }}.{{ $lessonIndex++ }}. {{ $lesson->title }}
@@ -81,7 +81,7 @@
                                         </div>
                                     </div>
 
-                                    <div class="col-lg-3 d-none d-sm-block text-right my-auto">
+                                    <div class="col-lg-3 col-md-2 col-sm-3 d-none d-sm-block text-right my-auto">
                                         <div class="lesson-info-time">
                                             <i class="fa fa-clock-o"></i>
                                             {{ formatSeconds($lesson->duration) }}
