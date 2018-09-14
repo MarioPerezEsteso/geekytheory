2b97ee165a60b12b9774e4398925c99825b7372d
Mario Pérez Esteso <mario@geekytheory.com>
2018-09-14 17:33:44 +0000

    feat(lesson): show technology icon in every lesson

--- resources/views/web/lessons/partials/list.blade.php
+++ resources/views/web/lessons/partials/list.blade.php
@@ -11,33 +11,30 @@
         @foreach($chapter->lessons as $lesson)
             <div class="block-footer border-top">
                 <div class="row h-100">
-                    <div class="col-lg-9 col-md-10 col-sm-9 col-12">
+                    <div class="col-lg-1 col-md-2 col-2 my-auto">
+                        {{ $chapter->order }}.{{ $lessonIndex++ }}
+                    </div>
+                    <div class="col-lg-1 col-md-2 col-2 my-auto">
+                        @if ($lesson->completed === true)
+                            <i class="lesson-completed-icon fa fa-check-circle"></i>
+                        @elseif (!empty($lesson->icon))
+                            <img class="lesson-technology-img" src="{{ \App\Http\Controllers\ImageManagerController::getPublicImageUrl($lesson->icon, false, true) }}"/>
+                        @endif
+                    </div>
+                    <div class="col-8 col-lg-10 my-auto">
                         <div class="row">
-                            <div class="col-lg-1 col-md-2 col-3 my-auto">
-                                @if (!empty($lesson->icon))
-                                    <img class="lesson-icon" src="{{ \App\Http\Controllers\ImageManagerController::getPublicImageUrl($lesson->icon, false, true) }}"/>
-                                    @if ($lesson->completed === true)
-                                        <i class="lesson-completed-icon lesson-completed-icon-over-icon fa fa-check-circle"></i>
-                                    @endif
-                                @else
-                                    @if ($lesson->completed === true)
-                                        <i class="lesson-completed-icon fa fa-check-circle"></i>
-                                    @endif
-                                @endif
-                            </div>
-                            <div class="col-9 col-lg-11 my-auto">
-                                <a href="{{ route('course.lesson', ['courseSlug' => $course->slug, 'lessonSlug' => $lesson->slug]) }}"
-                                   class="lesson-link">
-                                    {{ $chapter->order }}.{{ $lessonIndex++ }}. {{ $lesson->title }}
+                            <div class="col-12">
+                                <a href="{{ route('course.lesson', ['courseSlug' => $course->slug, 'lessonSlug' => $lesson->slug]) }}" class="lesson-link">
+                                    {{ $lesson->title }}
                                 </a>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-2 col-sm-3 d-none d-sm-block text-right my-auto">
-                        <div class="lesson-info-time">
-                            <i class="fa fa-clock-o"></i>
-                            {{ formatSeconds($lesson->duration) }}
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="lesson-info-time">
+                                    <i class="fa fa-clock-o"></i> {{ formatSeconds($lesson->duration) }}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
