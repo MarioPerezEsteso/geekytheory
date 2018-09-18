6a97828d7c9e4b5242cc03c766d80a0b2a1b9283
Mario Pérez Esteso <mario@geekytheory.com>
2018-09-18 19:25:47 +0000

    feat(lessonTracking): create policy to mark lesson as started

--- app/Policies/LessonPolicy.php
+++ app/Policies/LessonPolicy.php
@@ -11,6 +11,22 @@ class LessonPolicy
     use HandlesAuthorization;
 
     /**
+     * Policy to check if a user can mark a lesson as started.
+     *
+     * @param User $user
+     * @param Lesson $lesson
+     * @return bool
+     */
+    public function start(User $user, Lesson $lesson)
+    {
+        if (!$lesson->isPremium()) {
+            return true;
+        }
+
+        return $user->hasSubscriptionActive();
+    }
+
+    /**
      * Policy to check if a user can mark a lesson as completed.
      *
      * @param User $user
