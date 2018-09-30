77b05dffb59ec492e9725d9fdf313c23b0cc9ec3
Mario Pérez Esteso <mario@geekytheory.com>
2018-09-30 14:48:41 +0000

    feat(navbar): setup navbar

--- resources/views/web/partials/navbar.blade.php
+++ resources/views/web/partials/navbar.blade.php
@@ -1,65 +1,40 @@
-<nav class="navbar navbar-expand-lg navbar--bold  navbar-transparent navbar-inverse bg-dark ">
+<nav class="navbar navbar-expand-lg navbar--bold navbar-inverse bg-base-2">
     <div class="container navbar-container">
         <!-- Brand/Logo -->
-        <a class="navbar-brand" href="https://preview.webpixels.io/boomerang-v3.6/index.html">
-            <img src="/assets/images/logo/logo-1-a.png" class="d-none d-lg-inline-block"
-                 alt="Boomerang">
-            <img src="/assets/images/logo/logo-1-a.png" class="d-lg-none" alt="Boomerang">
+        <a class="navbar-brand" href="{{ config('app.url') }}">
+            <img src="/assets/images/logo/logo-geeky-theory.png" class="d-none d-lg-inline-block"
+                 alt="Geeky Theory">
+            <img src="/assets/images/logo/logo-geeky-theory.png" class="d-lg-none" alt="Boomerang">
         </a>
 
         <div class="d-inline-block">
             <!-- Navbar toggler  -->
             <button class="navbar-toggler hamburger hamburger-js hamburger--spring"
-                    type="button" data-toggle="collapse" data-target="#navbar_main"
-                    aria-controls="navbarsExampleDefault" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                                        <span class="hamburger-box">
-                                            <span class="hamburger-inner"></span>
-                                        </span>
+                    type="button" data-toggle="collapse" data-target="#navbar_main">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
             </button>
         </div>
 
-        <div class="collapse navbar-collapse align-items-center justify-content-end"
-             id="navbar_main">
+        <div class="collapse navbar-collapse align-items-center justify-content-end" id="navbar_main">
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
-                    <a href="blog.html#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                       aria-expanded="false">Home</a>
-
-                    <ul class="dropdown-menu">
-                        <li class="dropdown">
-                            <a href="index.html" class="dropdown-item" data-toggle="dropdown"
-                               aria-haspopup="true" aria-expanded="false">Homepage</a>
-                            <a href="homepage-video.html" class="dropdown-item"
-                               data-toggle="dropdown" aria-haspopup="true"
-                               aria-expanded="false">Homepage: Video</a>
-                            <a href="homepage-image.html" class="dropdown-item"
-                               data-toggle="dropdown" aria-haspopup="true"
-                               aria-expanded="false">Homepage: Image</a>
-                        </li>
-                    </ul>
+                    <a href="{{ route('courses') }}" class="nav-link">Cursos</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a href="pricing.html" class="nav-link">Pricing</a>
+                    <a href="{{ route('blog') }}" class="nav-link">Blog</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a href="blog.html" class="nav-link">Blog</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="contact.html" class="nav-link">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a href="https://preview.webpixels.io/boomerang-v3.6/demos.html"
-                       class="nav-link">Demos</a>
+                    <a href="{{ route('login') }}" class="nav-link">Iniciar sesión</a>
                 </li>
             </ul>
         </div>
 
         <div class="pl-4 d-none d-lg-inline-block">
-            <a href="https://goo.gl/9BbH7R"
-               class="btn btn-styled btn-sm btn-base-5 text-uppercase btn-circle">
-                Purchase now
-                <span class="badge badge-pill bg-base-1">$23 USD</span> </a>
+            <a href="{{ route('pricing') }}" class="btn btn-styled btn-sm btn-base-5 text-uppercase btn-circle">
+                Comenzar ahora
+            </a>
         </div>
     </div>
 </nav>
