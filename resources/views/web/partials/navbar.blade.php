24db8ea909037e31d77c9bba65d5ebabdcb49a53
Mario Pérez Esteso <mario@geekytheory.com>
2018-10-02 21:12:19 +0000

    feat(navbar): add logic to navbar

--- resources/views/web/partials/navbar.blade.php
+++ resources/views/web/partials/navbar.blade.php
@@ -2,15 +2,13 @@
     <div class="container navbar-container">
         <!-- Brand/Logo -->
         <a class="navbar-brand" href="{{ config('app.url') }}">
-            <img src="/assets/images/logo/logo-geeky-theory.png" class="d-none d-lg-inline-block"
-                 alt="Geeky Theory">
+            <img src="/assets/images/logo/logo-geeky-theory.png" class="d-none d-lg-inline-block" alt="Geeky Theory">
             <img src="/assets/images/logo/logo-geeky-theory.png" class="d-lg-none" alt="Boomerang">
         </a>
 
         <div class="d-inline-block">
             <!-- Navbar toggler  -->
-            <button class="navbar-toggler hamburger hamburger-js hamburger--spring"
-                    type="button" data-toggle="collapse" data-target="#navbar_main">
+            <button class="navbar-toggler hamburger hamburger-js hamburger--spring" type="button" data-toggle="collapse" data-target="#navbar_main">
                 <span class="hamburger-box">
                     <span class="hamburger-inner"></span>
                 </span>
@@ -25,16 +23,20 @@
                 <li class="nav-item dropdown">
                     <a href="{{ route('blog') }}" class="nav-link">Blog</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a href="{{ route('login') }}" class="nav-link">Iniciar sesión</a>
-                </li>
+                @if(!isset($user))
+                    <li class="nav-item dropdown">
+                        <a href="{{ route('login') }}" class="nav-link">Iniciar sesión</a>
+                    </li>
+                @endif
             </ul>
         </div>
 
-        <div class="pl-4 d-none d-lg-inline-block">
-            <a href="{{ route('pricing') }}" class="btn btn-styled btn-sm btn-base-5 text-uppercase btn-circle">
-                Comenzar ahora
-            </a>
-        </div>
+        @if(!isset($user) || (isset($user) && !$user['premium']))
+            <div class="pl-4 d-none d-lg-inline-block">
+                <a href="{{ route('pricing') }}" class="btn btn-styled btn-sm btn-base-5 text-uppercase btn-circle">
+                    Comenzar ahora
+                </a>
+            </div>
+        @endif
     </div>
 </nav>
